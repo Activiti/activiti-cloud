5d0cf36e985398e5be23fc405ace9401d955bbbc
Ryan Dawson <ryan.dawson@alfresco.com>
2017-08-17 12:20:52 +0100

    query implementation

---
+++ src/test/java/org/activiti/cloud/query/ApplicationTests.java
@@ -0,0 +1,20 @@
+package org.activiti.cloud.query;
+
+import org.actviti.cloud.query.QueryApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = QueryApplication.class)
+@DirtiesContext
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() throws Exception {
+
+	}
+
+}
