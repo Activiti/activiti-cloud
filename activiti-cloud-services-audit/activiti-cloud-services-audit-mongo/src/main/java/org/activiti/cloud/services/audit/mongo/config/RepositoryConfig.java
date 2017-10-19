234af4f8d6ae313dfbd4fc0da0a79b0341f8215e
Elias Ricken de Medeiros <erdemedeiros@users.noreply.github.com>
2017-10-19 16:39:14 +0100

    Expose read-only REST API using Spring Data REST for audit Mongo (#2)

--- activiti-cloud-services-audit/activiti-cloud-services-audit-mongo/src/main/java/org/activiti/cloud/services/audit/mongo/config/RepositoryConfig.java
+++ activiti-cloud-services-audit/activiti-cloud-services-audit-mongo/src/main/java/org/activiti/cloud/services/audit/mongo/config/RepositoryConfig.java
@@ -11,17 +11,24 @@ import org.activiti.cloud.services.audit.mongo.events.TaskCreatedEventDocument;
 import org.activiti.cloud.services.audit.mongo.events.VariableCreatedEventDocument;
 import org.activiti.cloud.services.audit.mongo.events.VariableDeletedEventDocument;
 import org.activiti.cloud.services.audit.mongo.events.VariableUpdatedEventDocument;
+import org.activiti.cloud.services.audit.mongo.repository.EventsRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "org.activiti.cloud.services.audit.mongo")
+@EnableMongoRepositories(basePackageClasses = EventsRepository.class)
 public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.setBasePath("/v1");
+        
+        // Expose only repositories annotated with @RepositoryRestResource
+        config.setRepositoryDetectionStrategy(RepositoryDetectionStrategies.ANNOTATED);
+
         config.exposeIdsFor(ActivityCompletedEventDocument.class);
         config.exposeIdsFor(ActivityStartedEventDocument.class);
         config.exposeIdsFor(ProcessCompletedEventDocument.class);
