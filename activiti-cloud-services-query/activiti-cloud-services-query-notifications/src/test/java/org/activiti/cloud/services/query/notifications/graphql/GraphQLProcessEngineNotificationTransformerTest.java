969be2ba6565679d1ac27a38998836014c5c4a19
Igor Dianov <igor@dianov.com>
2018-01-09 18:44:31 -0800

    Commit activiti-cloud-services-query-notifications implementation

---
+++ activiti-cloud-services-query/activiti-cloud-services-query-notifications/src/test/java/org/activiti/cloud/services/query/notifications/graphql/GraphQLProcessEngineNotificationTransformerTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2017 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.services.query.notifications.graphql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.cloud.services.query.notifications.consumer.ProcessEngineNotificationTransformer;
+import org.activiti.cloud.services.query.notifications.model.ProcessEngineNotification;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GraphQLProcessEngineNotificationTransformerTest {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(GraphQLProcessEngineNotificationTransformerTest.class);
+
+    private static String engineEventAttributeKeys = "applicationName,processInstanceId,processDefinitionId";
+    private static String eventTypeKey = "eventType";
+
+    @Test
+    public void transform() throws JsonProcessingException {
+
+        List<Map<String, Object>> events = new ArrayList<Map<String, Object>>() {
+            private static final long serialVersionUID = 1L;
+        {
+            add(new GraphQLProcessEngineNotification() {
+                private static final long serialVersionUID = 1L;
+            {
+                put("applicationName","app");
+                put("processInstanceId","p1");
+                put("processDefinitionId","pd1");
+                put("eventType","type1");
+                put("executionId","e1");
+            }});
+
+            add(new GraphQLProcessEngineNotification() {
+                private static final long serialVersionUID = 1L;
+            {
+                put("applicationName","app");
+                put("processInstanceId","p1");
+                put("processDefinitionId","pd1");
+                put("eventType","type2");
+                put("executionId","e1");
+            }});
+
+            add(new GraphQLProcessEngineNotification() {
+                private static final long serialVersionUID = 1L;
+            {
+                put("applicationName","app");
+                put("processInstanceId","p1");
+                put("processDefinitionId","pd1");
+                put("eventType","type2");
+                put("executionId","e1");
+            }});
+
+            add(new GraphQLProcessEngineNotification() {
+                private static final long serialVersionUID = 1L;
+            {
+                put("applicationName","app1");
+                put("processInstanceId","p1");
+                put("processDefinitionId","pd1");
+                put("eventType","type1");
+                put("executionId","e1");
+            }});
+
+        }};
+
+        ProcessEngineNotificationTransformer mapper = new GraphQLProcessEngineNotificationTransformer(
+                Arrays.asList(engineEventAttributeKeys.split(",")), eventTypeKey
+        );
+
+        List<ProcessEngineNotification> notifications = mapper.transform(events);
+
+        LOGGER.info("\n{}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(notifications));
+
+        assertThat(notifications).hasSize(2);
+        assertThat(notifications.get(0).get("applicationName")).isEqualTo("app");
+        assertThat(notifications.get(0).keySet())
+            .containsOnly("processInstanceId","applicationName","processDefinitionId","type1","type2");
+        assertThat(notifications.get(0).get("type2")).asList().hasSize(2);
+
+        assertThat(notifications.get(1).get("applicationName")).isEqualTo("app1");
+        assertThat(notifications.get(1).keySet())
+            .containsOnly("processInstanceId","applicationName","processDefinitionId","type1");
+        assertThat(notifications.get(1).get("type1")).asList().hasSize(1);
+
+    }
+
+}
