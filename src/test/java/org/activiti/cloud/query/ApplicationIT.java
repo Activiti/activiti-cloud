b03fd732a628232cfa6f26873035a4e0f90bcf0c
Ryan Dawson <ryandawson@cantab.net>
2018-02-14 16:40:47 +0000

    test without query or notification can run faster (#21)
    test without query or notification can run faster

--- src/test/java/org/activiti/cloud/query/ApplicationIT.java
+++ src/test/java/org/activiti/cloud/query/ApplicationIT.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = QueryApplication.class)
 @DirtiesContext
-public class ApplicationTestIT {
+public class ApplicationIT {
 
 	@Test
 	public void contextLoads() throws Exception {
