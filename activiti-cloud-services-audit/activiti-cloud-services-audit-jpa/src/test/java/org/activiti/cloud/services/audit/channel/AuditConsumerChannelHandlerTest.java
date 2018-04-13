1d74890af95bc99c13a8194bf58dd05f808889e0
Constantin Ciobotaru <Constantin.Ciobotaru@ness.com>
2018-04-13 16:38:54 +0300

    1812 handle task cancelled event
    https://github.com/Activiti/Activiti/issues/1812

--- activiti-cloud-services-audit/activiti-cloud-services-audit-jpa/src/test/java/org/activiti/cloud/services/audit/channel/AuditConsumerChannelHandlerTest.java
+++ activiti-cloud-services-audit/activiti-cloud-services-audit-jpa/src/test/java/org/activiti/cloud/services/audit/channel/AuditConsumerChannelHandlerTest.java
@@ -16,14 +16,19 @@
 
 package org.activiti.cloud.services.audit.channel;
 
+import org.activiti.cloud.services.audit.converter.TaskJpaJsonConverter;
 import org.activiti.cloud.services.audit.events.IgnoredProcessEngineEvent;
-import org.activiti.cloud.services.audit.repository.EventsRepository;
 import org.activiti.cloud.services.audit.events.ProcessEngineEventEntity;
+import org.activiti.cloud.services.audit.events.TaskCancelledEventEntity;
+import org.activiti.cloud.services.audit.events.model.Task;
+import org.activiti.cloud.services.audit.repository.EventsRepository;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -55,5 +60,25 @@ public class AuditConsumerChannelHandlerTest {
         verify(eventsRepository).save(firstEvent);
         verify(eventsRepository).save(secondEvent);
         verify(eventsRepository, never()).save(ignoredProcessEngineEvent);
+    }
+
+    @Test
+    public void testReceiveTaskCancelledEvent() throws Exception {
+        //GIVEN
+
+        TaskCancelledEventEntity taskCancelledEventEntity = new TaskCancelledEventEntity();
+        Task task = new TaskJpaJsonConverter().convertToEntityAttribute(
+                "{\"id\":\"1\",\"createdDate\":1523945813114,\"priority\":50,\"status\":\"CANCELLED\"}");
+        FieldUtils.writeField(taskCancelledEventEntity, "task", task, true);
+        ProcessEngineEventEntity[] events = new ProcessEngineEventEntity[] {taskCancelledEventEntity};
+
+        //WHEN
+        handler.receive(events);
+
+        //THEN
+        assertThat(taskCancelledEventEntity.isIgnored()).isFalse();
+        assertThat(taskCancelledEventEntity.getTask()).isNotNull();
+        assertThat(taskCancelledEventEntity.getTask().getStatus()).isEqualTo("CANCELLED");
+        verify(eventsRepository).save(taskCancelledEventEntity);
     }
 }
