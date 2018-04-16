80c86c2622a792b0b5e32d855d7b56cd01e5cafa
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-16 13:33:18 +0100

    use starter for propagating metadata to eureka

--- src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationIT.java
+++ src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationIT.java
@@ -3,20 +3,15 @@ package org.activiti.cloud.runtime;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.activiti.cloud.services.metadata.MetadataService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RuntimeBundleApplication.class)
 @DirtiesContext
 public class RuntimeBundleApplicationIT {
-
-    @Autowired
-    private MetadataService metadataService;
 
     @Value("${spring.activiti.process-definition-location-prefix:}")
     private String procPath;
@@ -30,6 +25,5 @@ public class RuntimeBundleApplicationIT {
                                "file:/processes/");
         Assert.assertEquals(procPath,
                             ("file:" + System.getenv("ACT_RB_PROCESSES_PATH")));
-        Assert.assertNotNull(metadataService);
     }
 }
