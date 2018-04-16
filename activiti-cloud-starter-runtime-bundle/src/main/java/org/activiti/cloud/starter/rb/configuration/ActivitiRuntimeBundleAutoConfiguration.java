971839150ad8e39fd2ebd00ee8b3793beee47b39
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-16 13:30:49 +0100

    propagate metadata to eureka

--- activiti-cloud-starter-runtime-bundle/src/main/java/org/activiti/cloud/starter/rb/configuration/ActivitiRuntimeBundleAutoConfiguration.java
+++ activiti-cloud-starter-runtime-bundle/src/main/java/org/activiti/cloud/starter/rb/configuration/ActivitiRuntimeBundleAutoConfiguration.java
@@ -2,9 +2,15 @@ package org.activiti.cloud.starter.rb.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("org.activiti.cloud.services")
+@PropertySources({
+        @PropertySource("classpath:metadata.properties"),
+        @PropertySource("classpath:metadata-eureka.properties") //will have no effect when running without eureka
+})
 public class ActivitiRuntimeBundleAutoConfiguration {
 
 }
