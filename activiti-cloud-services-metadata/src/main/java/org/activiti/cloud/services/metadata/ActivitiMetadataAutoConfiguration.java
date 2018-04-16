aaba94fa64d4cebe264753731e514fbf75b15144
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-16 10:49:33 +0100

    make properties available to autoconfiguration

--- activiti-cloud-services-metadata/src/main/java/org/activiti/cloud/services/metadata/ActivitiMetadataAutoConfiguration.java
+++ activiti-cloud-services-metadata/src/main/java/org/activiti/cloud/services/metadata/ActivitiMetadataAutoConfiguration.java
@@ -3,11 +3,13 @@ package org.activiti.cloud.services.metadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnProperty(name = "activiti.cloud.services.metadata.enabled", matchIfMissing = true)
+@EnableConfigurationProperties(MetadataProperties.class)
 public class ActivitiMetadataAutoConfiguration {
 
     @Autowired
