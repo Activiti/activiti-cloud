efd18c4a3db755cabeb4c1a86537cb66a03cd840
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-16 12:45:19 +0100

    a way to automatically propagte any metadata to eureka

--- activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
+++ activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
@@ -2,10 +2,12 @@ package org.activiti.cloud.starter.query.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan({"org.activiti.cloud.services.query",
         "org.activiti.cloud.alfresco", "org.activiti.cloud.services.security","org.activiti.cloud.services.identity"})
+@PropertySource("classpath:metadata-eureka.properties") //includes the props for eureka
 public class ActivitiQueryAutoConfiguration {
 
 }
