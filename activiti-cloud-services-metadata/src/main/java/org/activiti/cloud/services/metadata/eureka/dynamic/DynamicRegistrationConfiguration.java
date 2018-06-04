e6b7d8e0e07d68e868210b7d5088d2729af0e371
Ryan Dawson <ryan.dawson@alfresco.com>
2018-06-04 09:50:15 +0100

    provide property to turn off dynamic registration

--- activiti-cloud-services-metadata/src/main/java/org/activiti/cloud/services/metadata/eureka/dynamic/DynamicRegistrationConfiguration.java
+++ activiti-cloud-services-metadata/src/main/java/org/activiti/cloud/services/metadata/eureka/dynamic/DynamicRegistrationConfiguration.java
@@ -4,12 +4,14 @@ import com.netflix.appinfo.ApplicationInfoManager;
 import org.activiti.cloud.services.metadata.MetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 @ConditionalOnClass(ApplicationInfoManager.class)
+@ConditionalOnProperty(name = "activiti.cloud.services.metadata.eureka.dynamic.enabled", matchIfMissing = true)
 // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
 public class DynamicRegistrationConfiguration {
 
