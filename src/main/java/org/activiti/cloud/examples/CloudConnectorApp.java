eaab5144ce5041313d1f9cf18230e80014246546
Ryan Dawson <ryan.dawson@alfresco.com>
2018-07-24 13:54:13 +0100

    componentscan like in dummytwitter seems to be necessary

--- src/main/java/org/activiti/cloud/examples/CloudConnectorApp.java
+++ src/main/java/org/activiti/cloud/examples/CloudConnectorApp.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableActivitiCloudConnector
-@ComponentScan({"org.activiti.cloud.connectors.starter"})
+@ComponentScan({"org.activiti.cloud.connectors.starter","org.activiti.cloud.examples"})
 public class CloudConnectorApp {
 
     public static void main(String[] args) {
