eef11fbce98cd6506a528e66fc4a4325e0f61370
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-13 15:00:11 +0100

    swagger conf now in service level

--- src/main/java/org/activiti/cloud/runtime/RuntimeBundleApplication.java
+++ src/main/java/org/activiti/cloud/runtime/RuntimeBundleApplication.java
@@ -1,15 +1,11 @@
 package org.activiti.cloud.runtime;
 
 import org.activiti.cloud.starter.rb.configuration.ActivitiRuntimeBundle;
-import org.activiti.cloud.starter.rb.configuration.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ActivitiRuntimeBundle
-@Import(SwaggerConfig.class) //TODO: this is temporary, will apply this in service layer
 public class RuntimeBundleApplication {
 
     public static void main(String[] args) {
