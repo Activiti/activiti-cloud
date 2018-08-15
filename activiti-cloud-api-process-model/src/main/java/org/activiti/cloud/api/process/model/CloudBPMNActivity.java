05e2b988e4ce1efe2179714103b02fcf863019f1
salaboy <salaboy@gmail.com>
2018-08-15 10:03:32 +0100

    initial commit, only apis

---
+++ activiti-cloud-api-process-model/src/main/java/org/activiti/cloud/api/process/model/CloudBPMNActivity.java
@@ -0,0 +1,9 @@
+package org.activiti.cloud.api.process.model;
+
+import org.activiti.api.process.model.BPMNActivity;
+import org.activiti.cloud.api.model.shared.CloudRuntimeEntity;
+
+public interface CloudBPMNActivity extends CloudRuntimeEntity,
+        BPMNActivity {
+
+}
---
+++ activiti-cloud-api-process-model/src/main/java/org/activiti/cloud/api/process/model/CloudProcessInstance.java
@@ -0,0 +1,9 @@
+package org.activiti.cloud.api.process.model;
+
+import org.activiti.api.process.model.ProcessInstance;
+import org.activiti.cloud.api.model.shared.CloudRuntimeEntity;
+
+public interface CloudProcessInstance extends CloudRuntimeEntity,
+        ProcessInstance {
+
+}
