8380bed5066d95a2ca9cb07bb17d147597330a43
salaboy <salaboy@gmail.com>
2018-12-27 18:36:26 +0000

    adding new event types to handlers check

--- activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/events/handlers/QueryEventHandlerContextIT.java
+++ activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/events/handlers/QueryEventHandlerContextIT.java
@@ -133,6 +133,8 @@ public class QueryEventHandlerContextIT {
                 TaskRuntimeEvent.TaskEvents.TASK_UPDATED.name(),
                 TaskCandidateUserEvent.TaskCandidateUserEvents.TASK_CANDIDATE_USER_ADDED.name(),
                 TaskCandidateGroupEvent.TaskCandidateGroupEvents.TASK_CANDIDATE_GROUP_ADDED.name(),
+                TaskCandidateUserEvent.TaskCandidateUserEvents.TASK_CANDIDATE_USER_REMOVED.name(),
+                TaskCandidateGroupEvent.TaskCandidateGroupEvents.TASK_CANDIDATE_GROUP_REMOVED.name(),
                 VariableEvent.VariableEvents.VARIABLE_CREATED.name(),
                 VariableEvent.VariableEvents.VARIABLE_UPDATED.name(),
                 VariableEvent.VariableEvents.VARIABLE_DELETED.name()
@@ -153,6 +155,8 @@ public class QueryEventHandlerContextIT {
         assertThat(handlers.get(TaskRuntimeEvent.TaskEvents.TASK_UPDATED.name())).isInstanceOf(TaskUpdatedEventHandler.class);
         assertThat(handlers.get(TaskCandidateUserEvent.TaskCandidateUserEvents.TASK_CANDIDATE_USER_ADDED.name())).isInstanceOf(TaskCandidateUserAddedEventHandler.class);
         assertThat(handlers.get(TaskCandidateGroupEvent.TaskCandidateGroupEvents.TASK_CANDIDATE_GROUP_ADDED.name())).isInstanceOf(TaskCandidateGroupAddedEventHandler.class);
+        assertThat(handlers.get(TaskCandidateUserEvent.TaskCandidateUserEvents.TASK_CANDIDATE_USER_REMOVED.name())).isInstanceOf(TaskCandidateUserRemovedEventHandler.class);
+        assertThat(handlers.get(TaskCandidateGroupEvent.TaskCandidateGroupEvents.TASK_CANDIDATE_GROUP_REMOVED.name())).isInstanceOf(TaskCandidateGroupRemovedEventHandler.class);
         assertThat(handlers.get(VariableEvent.VariableEvents.VARIABLE_CREATED.name())).isInstanceOf(VariableCreatedEventHandler.class);
         assertThat(handlers.get(VariableEvent.VariableEvents.VARIABLE_UPDATED.name())).isInstanceOf(VariableUpdatedEventHandler.class);
         assertThat(handlers.get(VariableEvent.VariableEvents.VARIABLE_DELETED.name())).isInstanceOf(VariableDeletedEventHandler.class);
