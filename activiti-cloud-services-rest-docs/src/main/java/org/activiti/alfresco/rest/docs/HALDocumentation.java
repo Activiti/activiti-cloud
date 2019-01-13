3b5c68e1d2caad4b16b20167e56fe2323aa5a5e8
Elias Ricken de Medeiros <erdemedeiros@users.noreply.github.com>
2019-01-13 11:46:35 +0000

    Add descriptors to cover paged process instances (#119)
    * Add descriptors to cover paged process instances
    
    * Add descriptors to cover paged tasks
    
    * Add descriptors to cover paged variables
    
    * Fix typo
    
    * Add support to unpaged variables (used by runtime bundle)

--- activiti-cloud-services-rest-docs/src/main/java/org/activiti/alfresco/rest/docs/HALDocumentation.java
+++ activiti-cloud-services-rest-docs/src/main/java/org/activiti/alfresco/rest/docs/HALDocumentation.java
@@ -21,10 +21,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.restdocs.hypermedia.LinkDescriptor;
+import org.springframework.restdocs.hypermedia.LinksSnippet;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 
@@ -56,6 +58,24 @@ public class HALDocumentation {
 
     public static LinkDescriptor selfLink() {
         return linkWithRel("self").description("URL called to get this result");
+    }
+
+    public static LinksSnippet pageLinks() {
+        return links(selfLink(),
+              linkWithRel("last")
+                      .description("The last page")
+                      .optional(),
+              linkWithRel("prev")
+                      .description("The previous page")
+                      .optional(),
+              linkWithRel("first")
+                      .description("The first page")
+                      .optional(),
+              linkWithRel("next")
+                      .description("The next page")
+                      .optional()
+
+        );
     }
 
     public static ResponseFieldsSnippet pagedProcessDefinitionFields() {
@@ -104,4 +124,197 @@ public class HALDocumentation {
                         .optional()
         );
     }
+
+    public static ResponseFieldsSnippet pagedProcessInstanceFields() {
+        return pagedResponseFields(
+                subsectionWithPath("_embedded.processInstances")
+                        .description("List of process instances"),
+                subsectionWithPath("_embedded.processInstances.[].id")
+                        .description("The process instance id"),
+                subsectionWithPath("_embedded.processInstances.[].processDefinitionId")
+                        .description("The id of related process definition"),
+                subsectionWithPath("_embedded.processInstances.[].status")
+                        .description("The process instance status"),
+                subsectionWithPath("_embedded.processInstances.[].name")
+                        .description("The process instance name")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].serviceName")
+                        .description("The name of the service where this process instance comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].serviceFullName")
+                        .description("The full name of the service where this process instance comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].serviceVersion")
+                        .description("The version of the service where this process instance comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].serviceType")
+                        .description("The type of the service where this process instance comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].appName")
+                        .description("The name of the application where this process instance comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.processInstances.[].appVersion")
+                        .description("The version of the application where this process instance comes from")
+                        .type(String.class)
+                        .optional()
+        );
+    }
+
+    public static ResponseFieldsSnippet pagedTasksFields() {
+        return pagedResponseFields(
+                subsectionWithPath("_embedded.tasks")
+                        .description("List of tasks"),
+                subsectionWithPath("_embedded.tasks.[].id")
+                        .description("The task id"),
+                subsectionWithPath("_embedded.tasks.[].processDefinitionId")
+                        .description("The id of related process definition")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].processInstanceId")
+                        .description("The id of related process instance")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].assignee")
+                        .description("The task assignee")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].description")
+                        .description("The task description")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].createdDate")
+                        .description("The date where the task has been created"),
+                subsectionWithPath("_embedded.tasks.[].dueDate")
+                        .description("The date where the task is due")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].claimedDate")
+                        .description("The date where the task has been claimed")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].priority")
+                        .description("The task priority"),
+                subsectionWithPath("_embedded.tasks.[].status")
+                        .description("The task status"),
+                subsectionWithPath("_embedded.tasks.[].name")
+                        .description("The task name"),
+                subsectionWithPath("_embedded.tasks.[].serviceName")
+                        .description("The name of the service where this task comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].serviceFullName")
+                        .description("The full name of the service where this task comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].serviceVersion")
+                        .description("The version of the service where this task comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].serviceType")
+                        .description("The type of the service where this task comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].appName")
+                        .description("The name of the application where this task comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.tasks.[].appVersion")
+                        .description("The version of the application where this task comes from")
+                        .type(String.class)
+                        .optional()
+        );
+    }
+
+    public static ResponseFieldsSnippet pagedVariablesFields() {
+        return pagedResponseFields(
+                subsectionWithPath("_embedded.variables")
+                        .description("List of variables"),
+                subsectionWithPath("_embedded.variables.[].processInstanceId")
+                        .description("The id of related process instance"),
+                subsectionWithPath("_embedded.variables.[].taskId")
+                        .description("The id of related task")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].name")
+                        .description("The variable name"),
+                subsectionWithPath("_embedded.variables.[].value")
+                        .description("The variable value"),
+                subsectionWithPath("_embedded.variables.[].type")
+                        .description("The variable type"),
+                subsectionWithPath("_embedded.variables.[].serviceName")
+                        .description("The name of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceFullName")
+                        .description("The full name of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceVersion")
+                        .description("The version of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceType")
+                        .description("The type of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].appName")
+                        .description("The name of the application where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].appVersion")
+                        .description("The version of the application where this variable comes from")
+                        .type(String.class)
+                        .optional()
+        );
+    }
+
+    public static ResponseFieldsSnippet unpagedVariableFields() {
+        return responseFields(
+                subsectionWithPath("_embedded.variables")
+                        .description("List of variables"),
+                subsectionWithPath("_embedded.variables.[].processInstanceId")
+                        .description("The id of related process instance"),
+                subsectionWithPath("_embedded.variables.[].taskId")
+                        .description("The id of related task")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].name")
+                        .description("The variable name"),
+                subsectionWithPath("_embedded.variables.[].value")
+                        .description("The variable value"),
+                subsectionWithPath("_embedded.variables.[].type")
+                        .description("The variable type"),
+                subsectionWithPath("_embedded.variables.[].serviceName")
+                        .description("The name of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceFullName")
+                        .description("The full name of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceVersion")
+                        .description("The version of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].serviceType")
+                        .description("The type of the service where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].appName")
+                        .description("The name of the application where this variable comes from")
+                        .type(String.class)
+                        .optional(),
+                subsectionWithPath("_embedded.variables.[].appVersion")
+                        .description("The version of the application where this variable comes from")
+                        .type(String.class)
+                        .optional()
+        );
+    }
+
 }
