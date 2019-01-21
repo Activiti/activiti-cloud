532acf5536ce6a4e3b6f6d257a9627c9ab1d1f5e
Igor Dianov <igor@dianov.com>
2019-01-20 23:50:40 -0800

    feat: (Starter) add CORS auto-configuration

---
+++ starter/src/main/java/org/activiti/cloud/notifications/graphql/starter/ActivitiNotificationsGraphQLStarterAutoConfiguration.java
@@ -0,0 +1,34 @@
+package org.activiti.cloud.notifications.graphql.starter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ActivitiNotificationsGraphQLStarterAutoConfiguration {
+	
+    @Configuration
+    public class DefaultActivitiNotificationsGraphQLStarterConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean
+		public WebMvcConfigurer graphQLBackendCorsConfiguration() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("*")
+							.allowedMethods(HttpMethod.GET.toString(), 
+										    HttpMethod.POST.toString(),
+										    HttpMethod.PUT.toString(), 
+										    HttpMethod.DELETE.toString(), 
+										    HttpMethod.OPTIONS.toString());
+				}
+			};
+		}
+    }    	
+
+}
