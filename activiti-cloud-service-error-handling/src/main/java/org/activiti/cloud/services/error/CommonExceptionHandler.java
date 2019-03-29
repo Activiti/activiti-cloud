893fc579d150e6c317b72a222b4ca485d480c1a9
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-03-29 10:38:09 +0000

    Create new module with common exception handler

---
+++ activiti-cloud-service-error-handling/src/main/java/org/activiti/cloud/services/error/CommonExceptionHandler.java
@@ -0,0 +1,26 @@
+package org.activiti.cloud.services.error;
+
+import org.activiti.api.model.shared.model.ActivitiError;
+import org.activiti.api.runtime.model.impl.ActivitiErrorImpl;
+import org.activiti.core.common.spring.security.policies.ActivitiForbiddenException;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(ActivitiForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Resource<ActivitiError> handleAppException(ActivitiForbiddenException ex) {
+        return new Resource<>(new ActivitiErrorImpl(HttpStatus.FORBIDDEN, ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Resource<ActivitiError> handleAppException(IllegalStateException ex) {
+        return new Resource<>(new ActivitiErrorImpl(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+}
