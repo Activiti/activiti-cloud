5c9daf4e183c59c0848224967db86017b19ec196
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-04-12 11:51:02 +0100

    Add CommonExceptionHandlerQuery

---
+++ activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/CommonExceptionHandlerQuery.java
@@ -0,0 +1,25 @@
+package org.activiti.cloud.services.query.rest;
+
+import org.activiti.api.model.shared.model.ActivitiErrorMessage;
+import org.activiti.api.runtime.model.impl.ActivitiErrorMessageImpl;
+import org.activiti.core.common.spring.security.policies.ActivitiForbiddenException;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice
+public class CommonExceptionHandlerQuery {
+
+    // TODO: 12/04/2019 this exception handler should be moved to activiti-cloud-service-common
+    @ExceptionHandler(ActivitiForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Resource<ActivitiErrorMessage> handleAppException(ActivitiForbiddenException ex, HttpServletResponse response) {
+        response.setContentType("application/json");
+        return new Resource<>(new ActivitiErrorMessageImpl(HttpStatus.FORBIDDEN.value(), ex.getMessage()));
+    }
+}
+
