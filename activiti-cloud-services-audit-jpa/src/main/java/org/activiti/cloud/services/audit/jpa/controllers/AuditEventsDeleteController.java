c2a4f843ccff845d3cf44be0829241534ef67f6e
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-04-18 18:20:36 +0100

    Ensure property propagation

--- activiti-cloud-services-audit-jpa/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsDeleteController.java
+++ activiti-cloud-services-audit-jpa/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsDeleteController.java
@@ -1,12 +1,14 @@
 package org.activiti.cloud.services.audit.jpa.controllers;
 
 import org.activiti.cloud.api.model.shared.events.CloudRuntimeEvent;
+import org.activiti.cloud.common.properties.CommonPropertiesAutoConfiguration;
 import org.activiti.cloud.services.audit.api.assembler.EventResourceAssembler;
 import org.activiti.cloud.services.audit.api.converters.APIEventToEntityConverters;
 import org.activiti.cloud.services.audit.api.resources.EventsRelProvider;
 import org.activiti.cloud.services.audit.jpa.events.AuditEventEntity;
 import org.activiti.cloud.services.audit.jpa.repository.EventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
@@ -19,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@ConditionalOnProperty(name = "activiti.rest.enable-deletion", havingValue = "true")
+@ConditionalOnProperty(name = "activiti.rest.enable-deletion", matchIfMissing = true)
+@ConditionalOnBean(CommonPropertiesAutoConfiguration.class)
 @RestController
 @RequestMapping(
         value = "/admin/v1/" + EventsRelProvider.COLLECTION_RESOURCE_REL,
