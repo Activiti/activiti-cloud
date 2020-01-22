221ccb01d5da6be60daa301a677baf5b5f1a4097
Alex Merkulov <almerico@gmail.com>
2020-01-22 15:33:22 +0200

    Update MQServiceTaskIT.java

--- activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/MQServiceTaskIT.java
+++ activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/MQServiceTaskIT.java
@@ -136,6 +136,7 @@ public class MQServiceTaskIT {
                                false);
 
         //engine can resolve annotated pojo in var to correct type but not without annotation
+        //temporary comment to unblock propogation
         assertThat(updatedVariables.get("customPojo").getClass()).isEqualTo(ObjectNode.class);
         assertThat(updatedVariables.get("customPojoAnnotated").getClass()).isEqualTo(CustomPojoAnnotated.class);
 
