352ebf3437c39f8ba51b5034293aa2790c53d459
Igor Dianov <igdianov@users.noreply.github.com>
2020-04-01 02:06:27 -0700

    feat(AAE-1291): add multi instance cloud connector and test error connector scenarios (#919)
    * feat(AAE-1291): add multi instance cloud connector test scenario
    
    * fix: add connector error propagation test scenario

--- runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/ProcessInstanceConnectors.java
+++ runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/ProcessInstanceConnectors.java
@@ -16,23 +16,30 @@
 
 package org.activiti.cloud.qa.story;
 
+import static org.activiti.api.process.model.events.IntegrationEvent.IntegrationEvents.INTEGRATION_ERROR_RECEIVED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.awaitility.Awaitility.await;
 
 import java.util.Collection;
 
-import net.thucydides.core.annotations.Steps;
+import org.activiti.cloud.acc.core.steps.audit.AuditSteps;
+import org.activiti.cloud.acc.core.steps.query.ProcessQuerySteps;
 import org.activiti.cloud.acc.core.steps.runtime.ProcessRuntimeBundleSteps;
 import org.activiti.cloud.acc.core.steps.runtime.ProcessVariablesRuntimeBundleSteps;
 import org.activiti.cloud.acc.shared.steps.VariableBufferSteps;
 import org.activiti.cloud.api.model.shared.CloudVariableInstance;
+import org.activiti.cloud.api.model.shared.events.CloudRuntimeEvent;
 import org.activiti.cloud.api.process.model.CloudProcessInstance;
+import org.activiti.cloud.api.process.model.events.CloudIntegrationErrorReceivedEvent;
 import org.activiti.cloud.api.task.model.CloudTask;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.springframework.hateoas.Resources;
+
+import net.serenitybdd.core.Serenity;
+import net.thucydides.core.annotations.Steps;
 
 public class ProcessInstanceConnectors {
 
@@ -45,6 +52,12 @@ public class ProcessInstanceConnectors {
     @Steps
     private ProcessVariablesRuntimeBundleSteps processVariablesRuntimeBundleSteps;
 
+    @Steps
+    private ProcessQuerySteps processQuerySteps;
+
+    @Steps
+    private AuditSteps auditSteps;
+
     private CloudProcessInstance processInstance;
 
     @Given("the user provides a variable named $variableName with value $variableValue")
@@ -54,20 +67,31 @@ public class ProcessInstanceConnectors {
                                         variableValue);
     }
 
-    
+    @Given("the user provides an integer variable named $variableName with value $variableValue")
+    public void givenVariable(String variableName,
+                              Integer variableValue) {
+        variableBufferSteps.addVariable(variableName,
+                                        variableValue);
+    }
+
     @When("the user starts an instance of process called $processDefinitionKey with the provided variables")
     public void startProcessWithAvailableVariables(String processDefinitionKey) {
         processInstance = processRuntimeBundleSteps.startProcessWithVariables(processDefinitionKey,
                                                                               variableBufferSteps.availableVariables());
+
+        Serenity.setSessionVariable("processInstanceId").to(processInstance.getId());
     }
 
     @Then("the process instance has a task named $taskName")
     public void processHasTask(String taskName) {
+
+        String processInstanceId = Serenity.sessionVariableCalled("processInstanceId");
+
         await().untilAsserted(
                 () ->
                 {
                     assertThat(processInstance).isNotNull();
-                    Collection<CloudTask> tasks = processRuntimeBundleSteps.getTaskByProcessInstanceId(processInstance.getId());
+                    Collection<CloudTask> tasks = processRuntimeBundleSteps.getTaskByProcessInstanceId(processInstanceId);
                     assertThat(tasks).isNotNull();
                     assertThat(tasks)
                             .extracting(CloudTask::getName)
@@ -79,9 +103,12 @@ public class ProcessInstanceConnectors {
     @Then("the process instance has a variable named $variableName with value $variableValue")
     public void assertThatHasVariable(String variableName,
                                       String variableValue) {
-        assertThat(processInstance).isNotNull();
+
+        String processInstanceId = Serenity.sessionVariableCalled("processInstanceId");
+
+        assertThat(processInstanceId).isNotNull();
         await().untilAsserted(() -> {
-            Resources<CloudVariableInstance> processVariables = processVariablesRuntimeBundleSteps.getVariables(processInstance.getId());
+            Resources<CloudVariableInstance> processVariables = processVariablesRuntimeBundleSteps.getVariables(processInstanceId);
             assertThat(processVariables.getContent()).isNotNull();
             assertThat(processVariables.getContent())
                     .extracting(CloudVariableInstance::getName,
@@ -90,4 +117,38 @@ public class ProcessInstanceConnectors {
                                     variableValue));
         });
     }
+
+    @Then("the query process instance has an integer variable named $variableName with value $variableValue")
+    public void assertThatQueryHasVariable(String variableName,
+                                           Integer variableValue) {
+        String processInstanceId = Serenity.sessionVariableCalled("processInstanceId");
+
+        processQuerySteps.checkProcessInstanceHasVariableValue(processInstanceId,
+                                                               variableName,
+                                                               variableValue);
+    }
+
+    @Then("integration error event is emitted for the process")
+    public void verifyIntegrationEventsForProcesses() throws Exception {
+
+        String processId = Serenity.sessionVariableCalled("processInstanceId");
+
+        await().untilAsserted(() -> {
+            Collection<CloudRuntimeEvent> events = auditSteps.getEventsByProcessInstanceId(processId);
+
+            assertThat(events)
+                    .filteredOn(CloudIntegrationErrorReceivedEvent.class::isInstance)
+                    .isNotEmpty()
+                    .extracting("eventType",
+                                "errorMessage",
+                                "errorClassName"
+                    )
+                    .containsExactly(
+                                     tuple(INTEGRATION_ERROR_RECEIVED,
+                                           "TestErrorConnector",
+                                           "java.lang.RuntimeException"
+                                     ));
+        });
+    }
+
 }
