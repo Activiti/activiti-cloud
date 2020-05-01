e969654ff1d9d622f298d159f0bfaf61d6276cc7
Marcello Teodori <marcello.teodori@alfresco.com>
2020-05-01 01:21:06 +0100

    fixed keycloak test

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakTestApplication.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakTestApplication.java
@@ -1,17 +1,16 @@
 package org.activiti.cloud.services.identity.keycloak;
 
 import org.activiti.cloud.services.common.security.keycloak.config.CommonSecurityAutoConfiguration;
-import org.activiti.cloud.services.test.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication(exclude = {CommonSecurityAutoConfiguration.class, TestConfiguration.class})
+@SpringBootApplication(exclude = CommonSecurityAutoConfiguration.class)
 @EnableWebSecurity
 public class KeycloakTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KeycloakTestApplication.class,
-                              args);
+        SpringApplication.run(KeycloakTestApplication.class, args);
     }
+
 }
