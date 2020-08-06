4f7bf47fb6849f26d774ae3b5f8289176909e400
Alex Merkulov <almerico@gmail.com>
2020-08-06 15:31:02 +0300

    oracle syntax (#178)

--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/02-alter.oracle.schema.7.1.0-m9.sql
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/02-alter.oracle.schema.7.1.0-m9.sql
@@ -2,10 +2,10 @@ alter table process_instance
     add process_definition_name varchar(255);
 
 create index pi_processDefinitionName_idx on process_instance (process_definition_name);
-    
-update process_instance as pi
-  set process_definition_name = (select pd.name 
-                                    from process_definition as pd 
+
+update process_instance pi
+  set process_definition_name = (select pd.name
+                                    from process_definition as pd
                                     where pd.id = pi.process_definition_id);
 
 commit;
