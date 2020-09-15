c705a2f1fc9befc34291d383b34e28311351e79b
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-09-15 14:39:17 +0100

    Update postgresql update syntax

--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/06-alter.pg.schema.7.1.0-m10.sql
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/06-alter.pg.schema.7.1.0-m10.sql
@@ -3,7 +3,7 @@ alter table process_instance
 
 create index pi_completedDate_idx on process_instance (completed_date);
 
-update process_instance pi
+update process_instance as pi
     set completed_date = (select p_i.last_modified
-                             from process_instance p_i
+                             from process_instance as p_i
                              where p_i.id = pi.id);
