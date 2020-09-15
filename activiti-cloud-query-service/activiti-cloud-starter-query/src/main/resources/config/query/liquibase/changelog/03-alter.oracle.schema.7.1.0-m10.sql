0907b8bbf3f861e5137dbe8a3500eac4d94893f3
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-09-15 11:54:27 +0100

    Update completed_date to be same as last_modified in process_instance db table

--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/03-alter.oracle.schema.7.1.0-m10.sql
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/03-alter.oracle.schema.7.1.0-m10.sql
@@ -3,4 +3,9 @@ alter table process_instance
 
 create index pi_completedDate_idx on process_instance (completed_date);
 
+update process_instance pi
+    set completed_date = (select p_i.last_modified
+                             from process_instance p_i
+                             where p_i.id = pi.id);
+
 commit;
