9137123271e77e60b2672036394b5bd2305004a8
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-09-16 09:45:58 +0100

    Guard process_instance update logic and rename db files to resolve merge conflict

--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/05-alter.oracle.schema.7.1.0-m10.sql
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/05-alter.oracle.schema.7.1.0-m10.sql
@@ -6,6 +6,6 @@ create index pi_completedDate_idx on process_instance (completed_date);
 update process_instance pi
     set completed_date = (select p_i.last_modified
                              from process_instance p_i
-                             where p_i.id = pi.id);
+                             where (p_i.id = pi.id AND pi.status = 'COMPLETED'));
 
 commit;
