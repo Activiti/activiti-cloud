1ffa3e104ba97b7627f33daaeb36e36686c51dfb
gfertuso <giovanni.fertuso@alfresco.com>
2020-11-05 17:33:37 +0000

    removed unnecessary insert script

--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/07-alter.oracle.schema.7.1.0-m11.sql
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/resources/config/query/liquibase/changelog/07-alter.oracle.schema.7.1.0-m11.sql
@@ -5,9 +5,3 @@ create table application
     version                    varchar(255),
     primary key (id)
 );
-
-INSERT INTO application
-SELECT DISTINCT CONCAT(app_name, app_version), app_name, app_version
-FROM process_instance;
-
-commit;
