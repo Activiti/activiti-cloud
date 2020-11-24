15262301866e2197e934d4b8a3d27aad3b5195d7
Igor Dianov <igor@dianov.com>
2020-11-23 23:32:51 -0800

    fix: add activiti-cloud-services-query-liquibase module

---
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/java/org/activiti/cloud/services/query/liquibase/ActivitiCloudQueryLiquibaseAutoConfiguration.java
@@ -0,0 +1,10 @@
+package org.activiti.cloud.services.query.liquibase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config/query-liquibase.properties")
+public class ActivitiCloudQueryLiquibaseAutoConfiguration {
+
+}
