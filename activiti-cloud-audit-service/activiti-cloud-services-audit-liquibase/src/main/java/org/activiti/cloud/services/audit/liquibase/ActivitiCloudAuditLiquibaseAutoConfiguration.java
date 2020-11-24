034ccff1515fe2a942e75ce4fc6a77ab80973abc
Igor Dianov <igor@dianov.com>
2020-11-23 23:30:14 -0800

    fix(AAE-1012): add activiti-cloud-services-audit-liquibase module

---
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-liquibase/src/main/java/org/activiti/cloud/services/audit/liquibase/ActivitiCloudAuditLiquibaseAutoConfiguration.java
@@ -0,0 +1,16 @@
+package org.activiti.cloud.services.audit.liquibase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config/audit-liquibase.properties")
+public class ActivitiCloudAuditLiquibaseAutoConfiguration {
+
+//    LiquibaseAutoConfiguration d;
+//
+//    public Liquibase auditLiquibase() {
+//        return new Liquibase();
+//    }
+
+}
