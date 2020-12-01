47ac08c03b2ac33f5f82fa14d131b63020f21399
Igor Dianov <igor@dianov.com>
2020-11-30 16:01:30 -0800

    fix: add Audit Liquibase auto configuration unit test

---
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-liquibase/src/test/java/org/activiti/cloud/services/audit/liquibase/ActivitiCloudAuditLiquibaseAutoConfigurationTest.java
@@ -0,0 +1,15 @@
+package org.activiti.cloud.services.audit.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootApplication
+class ActivitiCloudAuditLiquibaseAutoConfigurationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
