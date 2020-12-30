82ef0f22c86f42ae5e39e8c5c398eeef8f1d36d7
Igor Dianov <igor@dianov.com>
2020-12-30 15:03:42 -0800

    fix: clean up changes

--- activiti-cloud-messages-service/starters/hazelcast/src/test/java/org/activiti/cloud/starter/messages/test/hazelcast/HazelcastMessageStoreIT.java
+++ activiti-cloud-messages-service/starters/hazelcast/src/test/java/org/activiti/cloud/starter/messages/test/hazelcast/HazelcastMessageStoreIT.java
@@ -46,6 +46,9 @@ public class HazelcastMessageStoreIT extends AbstractMessagesCoreIntegrationTest
         public Config hazelcastConfig() {
             Config config = new Config();
 
+            config.getGroupConfig()
+                  .setName(HazelcastMessageStoreIT.class.getSimpleName());
+
             config.getCPSubsystemConfig()
                   .setCPMemberCount(3);
 
