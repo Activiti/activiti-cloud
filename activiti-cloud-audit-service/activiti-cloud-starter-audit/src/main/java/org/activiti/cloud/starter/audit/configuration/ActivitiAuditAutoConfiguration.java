1cc39aa8bcbf2946f5d42afa0b43185feacb3e62
Igor Dianov <igor@dianov.com>
2021-04-27 23:19:25 -0700

    fix: separate messaging properties for audit and query

--- activiti-cloud-audit-service/activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
+++ activiti-cloud-audit-service/activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
@@ -17,9 +17,11 @@ package org.activiti.cloud.starter.audit.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(AuditSwaggerConfig.class)
+@PropertySource("classpath:audit-messaging.properties")
 public class ActivitiAuditAutoConfiguration {
 
 }
--- activiti-cloud-query-service/activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
+++ activiti-cloud-query-service/activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
@@ -17,9 +17,11 @@ package org.activiti.cloud.starter.query.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import(QuerySwaggerConfig.class)
+@PropertySource("classpath:query-messaging.properties")
 public class ActivitiQueryAutoConfiguration {
 
 }
