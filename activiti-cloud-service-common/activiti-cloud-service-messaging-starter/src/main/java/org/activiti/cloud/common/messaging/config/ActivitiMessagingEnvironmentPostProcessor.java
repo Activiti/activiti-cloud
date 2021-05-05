4bc28e1749b8427d63fe1dcc97dbf17ee18043ee
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2021-05-05 20:21:57 +0200

    Create starter module including rabbit and kafka (#416)
    * Create starter module including rabbit and kafka
    
    Create base module to include both rabbit and kafka Spring Cloud Stream binders. `rabbit` is the default one.
    Ref https://github.com/Activiti/Activiti/issues/3625
    
    * Add Activiti environment variable
    
    * Fix parent
    
    * Rename module
    
    * Use post processor to set spring default binder

---
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-starter/src/main/java/org/activiti/cloud/common/messaging/config/ActivitiMessagingEnvironmentPostProcessor.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.common.messaging.config;
+
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+
+import java.util.Collections;
+import org.activiti.cloud.common.messaging.ActivitiCloudMessagingProperties.MessagingBroker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+public class ActivitiMessagingEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    protected static final String ACTIVITI_CLOUD_MESSAGING_BROKER_KEY = "activiti.cloud.messaging.broker";
+    protected static final String SPRING_CLOUD_STREAM_DEFAULT_BINDER_KEY = "spring.cloud.stream.default-binder";
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment,
+        SpringApplication application) {
+        final MessagingBroker messagingBroker = environment
+            .getProperty(ACTIVITI_CLOUD_MESSAGING_BROKER_KEY, MessagingBroker.class, MessagingBroker.rabbitmq);
+
+        environment
+            .getPropertySources()
+            .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource(
+                this.getClass().getSimpleName(),
+                Collections.singletonMap(SPRING_CLOUD_STREAM_DEFAULT_BINDER_KEY,
+                    resolveDefaultBinder(messagingBroker))));
+    }
+
+    private String resolveDefaultBinder(MessagingBroker messagingBroker) {
+        switch (messagingBroker) {
+            case kafka:
+                return "kafka";
+            case rabbitmq:
+            default:
+                return "rabbit";
+        }
+    }
+}
