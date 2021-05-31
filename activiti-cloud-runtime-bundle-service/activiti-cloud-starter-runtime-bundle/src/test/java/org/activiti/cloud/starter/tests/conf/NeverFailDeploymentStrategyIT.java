3518dff01e552d1105190f8e3693338dfb90c2a7
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2021-05-31 15:51:00 +0200

    test: reduce amount of logs produced by runtime bundle tests
    The file `ParsingError.bpmn` was added in order to test `NeverFailDeploymentStrategy`, making sure that even when an error occurs while parsing a bpmn file the application starts (ignoring the file that could not be parsed). However, as this file was added at the same folder as all the other processes, it's producing a stack trace every time one test starts the application. This change is moving `ParsingError.bpmn` to specific folder in a such way that the stack trace will be produced only once.

---
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/conf/NeverFailDeploymentStrategyIT.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.starter.tests.conf;
+
+import org.activiti.cloud.api.process.model.CloudProcessDefinition;
+import org.activiti.cloud.services.test.containers.KeycloakContainerApplicationInitializer;
+import org.activiti.cloud.services.test.containers.RabbitMQContainerApplicationInitializer;
+import org.activiti.cloud.starter.tests.helper.ProcessDefinitionRestTemplate;
+import org.activiti.cloud.starter.tests.runtime.RuntimeITConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    properties = {"spring.activiti.process-definition-location-prefix=classpath*:/invalid-processes/"},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource({"classpath:application-test.properties", "classpath:access-control.properties"})
+@ContextConfiguration(classes = RuntimeITConfiguration.class,
+    initializers = {RabbitMQContainerApplicationInitializer.class, KeycloakContainerApplicationInitializer.class})
+public class NeverFailDeploymentStrategyIT {
+
+    @Autowired
+    private ProcessDefinitionRestTemplate processDefinitionRestTemplate;
+
+    @Test
+    public void rb_should_startEven_when_itFailsToParseSomeProcessDefinition() {
+        //when
+        ResponseEntity<PagedModel<CloudProcessDefinition>> processDefinitions = processDefinitionRestTemplate.getProcessDefinitions();
+
+        //then
+        //spring.activiti.process-definition-location-prefix points to a folder containing only an invalid process definition
+        //so no process definitions are expected
+        assertThat(processDefinitions.getBody()).isNotNull();
+        assertThat(processDefinitions.getBody().getContent()).isEmpty();
+    }
+}
