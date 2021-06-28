a31df65190d09351b13ba903669afaa3169936fc
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2021-06-28 19:16:19 +0200

    Update test to check chunks

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/listeners/CloudProcessDeployedProducerTest.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/listeners/CloudProcessDeployedProducerTest.java
@@ -15,6 +15,18 @@
  */
 package org.activiti.cloud.services.events.listeners;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.activiti.api.process.model.ProcessDefinition;
 import org.activiti.api.process.model.events.ProcessDeployedEvent;
 import org.activiti.api.runtime.event.impl.ProcessDeployedEventImpl;
@@ -24,29 +36,24 @@ import org.activiti.cloud.api.model.shared.impl.events.CloudRuntimeEventImpl;
 import org.activiti.cloud.api.process.model.events.CloudProcessDeployedEvent;
 import org.activiti.cloud.services.events.ProcessEngineChannels;
 import org.activiti.cloud.services.events.configuration.RuntimeBundleProperties;
+import org.activiti.cloud.services.events.configuration.RuntimeBundleProperties.RuntimeBundleEventsProperties;
 import org.activiti.cloud.services.events.converter.RuntimeBundleInfoAppender;
 import org.activiti.cloud.services.events.message.MessageBuilderAppenderChain;
 import org.activiti.cloud.services.events.message.RuntimeBundleMessageBuilderFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
-
+@ExtendWith(MockitoExtension.class)
 public class CloudProcessDeployedProducerTest {
 
-    @InjectMocks
     private CloudProcessDeployedProducer processDeployedProducer;
 
     @Mock
@@ -72,32 +79,38 @@ public class CloudProcessDeployedProducerTest {
 
     @BeforeEach
     public void setUp() {
-        initMocks(this);
         when(producer.auditProducer()).thenReturn(auditProducer);
         when(runtimeBundleMessageBuilderFactory.create()).thenReturn(messageBuilderAppenderChain);
+        final RuntimeBundleEventsProperties eventsProperties = new RuntimeBundleEventsProperties();
+        eventsProperties.setChunkSize(2);
+        properties.setEventsProperties(eventsProperties);
+        processDeployedProducer = new CloudProcessDeployedProducer(runtimeBundleInfoAppender, producer, runtimeBundleMessageBuilderFactory, properties);
     }
 
     @Test
-    public void shouldSendMessageWithDeployedProcessesWhenWebApplicationTypeIsServlet() {
+    public void should_sendMessageWithDeployedProcessesInChunks() {
         //given
         ProcessDefinition def1 = mock(ProcessDefinition.class);
         ProcessDefinition def2 = mock(ProcessDefinition.class);
-        List<ProcessDeployedEvent> processDeployedEventList = Arrays.asList(new ProcessDeployedEventImpl(def1,
-                                                                                                         "content1"),
-                                                                            new ProcessDeployedEventImpl(def2,
-                                                                                                         "content2"));
+        ProcessDefinition def3 = mock(ProcessDefinition.class);
+        List<ProcessDeployedEvent> processDeployedEventList = Arrays
+            .asList(
+                new ProcessDeployedEventImpl(def1, "content1"),
+                new ProcessDeployedEventImpl(def2, "content2"),
+                new ProcessDeployedEventImpl(def3, "content3"));
         given(messageBuilderAppenderChain.withPayload(any())).willReturn(MessageBuilder.withPayload(new CloudRuntimeEvent<?, ?>[2]));
 
         //when
         processDeployedProducer.sendProcessDeployedEvents(new ProcessDeployedEvents(processDeployedEventList));
 
         //then
-        verify(runtimeBundleInfoAppender, times(2)).appendRuntimeBundleInfoTo(any(CloudRuntimeEventImpl.class));
-        verify(messageBuilderAppenderChain).withPayload(messagePayloadCaptor.capture());
-        verify(auditProducer).send(any());
+        verify(runtimeBundleInfoAppender, times(3)).appendRuntimeBundleInfoTo(any(CloudRuntimeEventImpl.class));
+        verify(messageBuilderAppenderChain, times(2)).withPayload(messagePayloadCaptor.capture());
+        verify(auditProducer, times(2)).send(any());
 
-        List<CloudProcessDeployedEvent> cloudProcessDeployedEvents = Arrays.asList(messagePayloadCaptor.getValue())
-                .stream()
+        List<CloudRuntimeEvent<?, ?>[]> values = messagePayloadCaptor.getAllValues();
+        List<CloudProcessDeployedEvent> cloudProcessDeployedEvents = Arrays.stream(
+            values.get(0))
                 .map(CloudProcessDeployedEvent.class::cast)
                 .collect(Collectors.toList());
         assertThat(cloudProcessDeployedEvents)
@@ -107,5 +120,14 @@ public class CloudProcessDeployedProducerTest {
                                     "content1"),
                               tuple(def2,
                                     "content2"));
+
+        assertThat(Arrays.stream(
+            values.get(1))
+            .map(CloudProcessDeployedEvent.class::cast)
+            .collect(Collectors.toList()))
+            .extracting(CloudProcessDeployedEvent::getEntity,
+                CloudProcessDeployedEvent::getProcessModelContent)
+            .containsOnly(tuple(def3,
+                "content3"));
     }
 }
