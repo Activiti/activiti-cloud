781a76b9f20e9ddf5c2eccc151716b244a34ea6c
Igor Dianov <igor@dianov.com>
2021-08-25 18:19:19 -0700

    fix: use application header for message correlation id

--- activiti-cloud-messages-service/services/core/src/main/java/org/activiti/cloud/services/messages/core/correlation/Correlations.java
+++ activiti-cloud-messages-service/services/core/src/main/java/org/activiti/cloud/services/messages/core/correlation/Correlations.java
@@ -15,31 +15,29 @@
  */
 package org.activiti.cloud.services.messages.core.correlation;
 
-import static org.activiti.cloud.services.messages.core.integration.MessageEventHeaders.MESSAGE_EVENT_CORRELATION_KEY;
-import static org.activiti.cloud.services.messages.core.integration.MessageEventHeaders.MESSAGE_EVENT_NAME;
-import static org.activiti.cloud.services.messages.core.integration.MessageEventHeaders.SERVICE_FULL_NAME;
-
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 
+import static org.activiti.cloud.services.messages.core.integration.MessageEventHeaders.*;
+
 public class Correlations {
-    
+
     public static String getCorrelationId(Message<?> message) {
         MessageHeaders headers = message.getHeaders();
-        String serviceFullName = headers.get(SERVICE_FULL_NAME, String.class);
+        String appName = headers.get(APP_NAME, String.class);
         String messageEventName = headers.get(MESSAGE_EVENT_NAME, String.class);
         String messageCorrelationKey = headers.get(MESSAGE_EVENT_CORRELATION_KEY, String.class);
-        
+
         StringBuilder builder = new StringBuilder();
-        builder.append(serviceFullName)
+        builder.append(appName)
                .append(":")
                .append(messageEventName);
-               
+
         if (messageCorrelationKey != null) {
             builder.append(":")
                    .append(messageCorrelationKey);
         }
-        
-        return builder.toString();        
+
+        return builder.toString();
     }
 }
