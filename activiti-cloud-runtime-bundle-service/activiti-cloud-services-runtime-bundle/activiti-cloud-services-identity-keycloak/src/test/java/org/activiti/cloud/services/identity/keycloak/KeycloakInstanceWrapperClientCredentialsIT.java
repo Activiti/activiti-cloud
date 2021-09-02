e46008ea94f4511c8558d7162c1e7568d5986268
Igor Dianov <igor@dianov.com>
2021-09-01 22:19:28 -0700

    fix: add test container support for KeycloakInstanceWrapper

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakInstanceWrapperClientCredentialsIT.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-identity-keycloak/src/test/java/org/activiti/cloud/services/identity/keycloak/KeycloakInstanceWrapperClientCredentialsIT.java
@@ -15,23 +15,9 @@
  */
 package org.activiti.cloud.services.identity.keycloak;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.assertj.core.api.Assertions.*;
+@TestPropertySource("classpath:application-client-credentials.properties")
+public class KeycloakInstanceWrapperClientCredentialsIT extends KeycloakInstanceWrapperTestSupport {
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@TestPropertySource("classpath:application.properties")
-public class KeycloakInstanceWrapperIT {
-
-    @Autowired
-    private KeycloakInstanceWrapper keycloakInstanceWrapper;
-
-    @Test
-    public void shouldWireWrapper() {
-        assertThat(keycloakInstanceWrapper).isNotNull();
-        assertThat(keycloakInstanceWrapper.getRealm()).isNotNull();
-    }
 }
