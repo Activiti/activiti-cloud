6c12512602f6dbf2aba8b27e7d2c145dfe60d90a
Bassam Al-Sarori <balsarori@gmail.com>
2021-10-20 10:22:01 +0100

    Auto config to show info and health indicators by default (#493)
    * add auto config to show info and health indicators by default
    
    Signed-off-by: Bassam Al-Sarori <balsarori@gmail.com>
    
    * Fix typo
    
    Co-authored-by: Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>

---
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/monitoring/ActuatorHealthIndicatorsIT.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.starter.tests.monitoring;
+
+import org.activiti.cloud.services.common.security.keycloak.test.support.WithMockKeycloakUser;
+import org.activiti.cloud.services.test.containers.KeycloakContainerApplicationInitializer;
+import org.activiti.cloud.services.test.containers.RabbitMQContainerApplicationInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext
+@ContextConfiguration(initializers = {RabbitMQContainerApplicationInitializer.class, KeycloakContainerApplicationInitializer.class})
+public class ActuatorHealthIndicatorsIT {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockKeycloakUser(username = "admin", roles = "ACTIVITI_ADMIN", groups = "admins")
+    public void should_displayActuatorHealthIndicatorsDetails_when_authorized() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
+            .andExpect(status().is2xxSuccessful())
+            .andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+
+        assertThatJson(result.getResponse().getContentAsString())
+            .node("status").isEqualTo("UP")
+            .node("components.db.status").isEqualTo("UP")
+            .node("components.rabbit.status").isEqualTo("UP");
+    }
+
+    @Test
+    @WithMockKeycloakUser(username = "user", roles = "ACTIVITI_USER", groups = "users")
+    public void should_notDisplayActuatorHealthIndicatorsDetails_when_notAuthorized() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
+            .andExpect(status().is2xxSuccessful())
+            .andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+
+        assertThatJson(result.getResponse().getContentAsString())
+            .node("status").isEqualTo("UP")
+            .node("components.db.status").isAbsent()
+            .node("components.rabbit.status").isAbsent();
+    }
+
+    @Test
+    public void should_notDisplayActuatorHealthIndicatorsDetails_when_notAuthenticated() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
+            .andExpect(status().is2xxSuccessful())
+            .andReturn();
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
+
+        assertThatJson(result.getResponse().getContentAsString())
+            .node("status").isEqualTo("UP")
+            .node("components.db.status").isAbsent()
+            .node("components.rabbit.status").isAbsent();
+    }
+}
