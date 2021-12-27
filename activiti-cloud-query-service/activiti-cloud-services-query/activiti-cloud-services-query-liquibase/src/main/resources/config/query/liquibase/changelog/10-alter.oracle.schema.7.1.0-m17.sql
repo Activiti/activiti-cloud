4e43d5b2e6c46667beb0ed962faedb4083c91967
Igor Dianov <igor@dianov.com>
2021-12-27 13:23:14 -0800

    fix: add variable sequence declarations for Oracle DDL

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/10-alter.oracle.schema.7.1.0-m17.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/10-alter.oracle.schema.7.1.0-m17.sql
@@ -1,2 +1,14 @@
 alter table process_definition
     add category varchar(255);
+
+declare max_process_variable number;
+begin
+    select nvl(max(value), 0) + 1 into max_process_variable from process_variable;
+    execute immediate 'create sequence process_variable_sequence start with ' || max_process_variable || ' increment by 50';
+end;
+
+declare max_task_variable number;
+begin
+    select nvl(max(value), 0) + 1 into max_task_variable from task_variable;
+    execute immediate 'create sequence task_variable_sequence start with ' || max_task_variable || ' increment by 50';
+end;
