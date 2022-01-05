6a42fe5f74acf9bc4b3c281dbb832f2e92613882
Igor Dianov <igdianov@users.noreply.github.com>
2022-01-05 00:32:10 -0800

    fix: add missing slash to Oracle query Liquibase DDL for variable sequences (#543)

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/10-alter.oracle.schema.7.1.0-m17.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/10-alter.oracle.schema.7.1.0-m17.sql
@@ -6,9 +6,11 @@ begin
     select nvl(max(id), 0) + 1 into max_process_variable from process_variable;
     execute immediate 'create sequence process_variable_sequence start with ' || max_process_variable || ' increment by 50';
 end;
+/
 
 declare max_task_variable number;
 begin
     select nvl(max(id), 0) + 1 into max_task_variable from task_variable;
     execute immediate 'create sequence task_variable_sequence start with ' || max_task_variable || ' increment by 50';
 end;
+/
