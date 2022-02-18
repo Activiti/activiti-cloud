d9939c6ec7c79f345b500bc7d3baefdfae54ac6e
Nazareth Jimenez <njimenez@copyright.com>
2022-02-18 13:18:07 +0100

    3831 Juel Simulator Starter Codacy (#579)

--- activiti-cloud-modeling-service/activiti-cloud-starter-juel/src/test/java/org/activiti/cloud/starter/juel/swagger/JuelSwaggerIT.java
+++ activiti-cloud-modeling-service/activiti-cloud-starter-juel/src/test/java/org/activiti/cloud/starter/juel/swagger/JuelSwaggerIT.java
@@ -17,7 +17,6 @@ package org.activiti.cloud.starter.juel.swagger;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
