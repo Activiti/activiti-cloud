6064358ca02139c202b011e28b16e6517ac649ec
gfertuso <giovanni.fertuso@alfresco.com>
2022-02-28 12:09:43 +0000

    Activiti/Activiti#3844 Implement admin version of API ProcessInstanceTasks

---
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/ProcessInstanceEntityTasksAdminControllerIT.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.services.query.rest;
+
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
+import static org.activiti.cloud.services.query.rest.TestTaskEntityBuilder.buildDefaultTask;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collections;
+
+import org.activiti.api.runtime.conf.impl.CommonModelAutoConfiguration;
+import org.activiti.api.runtime.shared.identity.UserGroupManager;
+import org.activiti.api.runtime.shared.security.SecurityManager;
+import org.activiti.cloud.alfresco.argument.resolver.AlfrescoPageRequest;
+import org.activiti.cloud.alfresco.config.AlfrescoWebAutoConfiguration;
+import org.activiti.cloud.conf.QueryRestWebMvcAutoConfiguration;
+import org.activiti.cloud.services.query.app.repository.TaskRepository;
+import org.activiti.cloud.services.query.model.TaskEntity;
+import org.activiti.core.common.spring.security.policies.SecurityPoliciesManager;
+import org.activiti.core.common.spring.security.policies.conf.SecurityPoliciesProperties;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@WebMvcTest(ProcessInstanceTasksAdminController.class)
+@EnableSpringDataWebSupport
+@AutoConfigureMockMvc
+@Import({
+        QueryRestWebMvcAutoConfiguration.class,
+        CommonModelAutoConfiguration.class,
+        AlfrescoWebAutoConfiguration.class
+})
+@WithMockUser
+public class ProcessInstanceEntityTasksAdminControllerIT {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @MockBean
+    private UserGroupManager userGroupManager;
+
+    @MockBean
+    private SecurityManager securityManager;
+
+    @MockBean
+    private SecurityPoliciesManager securityPoliciesManager;
+
+    @MockBean
+    private SecurityPoliciesProperties securityPoliciesProperties;
+
+    @Test
+    public void getTasksShouldReturnAllResultsUsingAlfrescoMetadataWhenMediaTypeIsApplicationJson() throws Exception {
+        //given
+        TaskEntity taskEntity = buildDefaultTask();
+        
+        given(taskRepository.findAll(any(), any(Pageable.class)))
+                .willReturn(new PageImpl<>(Collections.singletonList(taskEntity),
+                                           new AlfrescoPageRequest(11, 10, PageRequest.of(0, 10)), 12));
+
+        //when
+        MvcResult result = mockMvc.perform(get("/admin/v1/process-instances/{processInstanceId}/tasks?skipCount=11&maxItems=10",
+                                               taskEntity.getProcessInstanceId())
+                                                   .accept(MediaType.APPLICATION_JSON))
+                //then
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThatJson(result.getResponse().getContentAsString())
+                .node("list.pagination.skipCount").isEqualTo(11)
+                .node("list.pagination.maxItems").isEqualTo(10)
+                .node("list.pagination.count").isEqualTo(1)
+                .node("list.pagination.hasMoreItems").isEqualTo(false)
+                .node("list.pagination.totalItems").isEqualTo(12);
+    }
+}
