7b46d59ed5f07121914267c0e99346394450b0e0
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2022-06-30 12:03:09 +0100

    Add builder for object

--- activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/identity/model/User.java
+++ activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/identity/model/User.java
@@ -19,60 +19,60 @@ import java.util.Objects;
 
 public class User {
 
-  private String id;
-  private String firstName;
-  private String lastName;
-  private String username;
-  private String email;
-  private String displayName;
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String displayName;
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-  public String getFirstName() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-  public String getLastName() {
-    return lastName;
-  }
+    public String getLastName() {
+        return lastName;
+    }
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getDisplayName() {
-    return displayName;
-  }
+    public String getDisplayName() {
+        return displayName;
+    }
 
-  public void setDisplayName(String displayName) {
-    this.displayName = displayName;
-  }
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -101,5 +101,63 @@ public class User {
             ", email='" + email + '\'' +
             ", displayName='" + displayName + '\'' +
             '}';
+    }
+
+    public class UserBuilder {
+
+        private String id;
+        private String firstName;
+        private String lastName;
+        private String username;
+        private String email;
+        private String displayName;
+
+        public UserBuilder(String id,
+            String firstName,
+            String lastName,
+            String username,
+            String email,
+            String displayName) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.username = username;
+            this.email = email;
+            this.displayName = displayName;
+        }
+
+        public UserBuilder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder displayName(String displayName) {
+            this.displayName = displayName;
+            return this;
+        }
+
+        public UserBuilder build() {
+            return new UserBuilder(id, firstName, lastName, username, email, displayName);
+        }
     }
 }
