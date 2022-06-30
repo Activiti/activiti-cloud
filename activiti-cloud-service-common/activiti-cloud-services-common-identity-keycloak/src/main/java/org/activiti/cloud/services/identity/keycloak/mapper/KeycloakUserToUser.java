32ace7f87097f07a5fb5ea8213920ecf296cd34f
Miguel Ruiz <miguel.ruiz.dev@gmail.com>
2022-06-30 14:28:00 +0100

    clean up

--- activiti-cloud-service-common/activiti-cloud-services-common-identity-keycloak/src/main/java/org/activiti/cloud/services/identity/keycloak/mapper/KeycloakUserToUser.java
+++ activiti-cloud-service-common/activiti-cloud-services-common-identity-keycloak/src/main/java/org/activiti/cloud/services/identity/keycloak/mapper/KeycloakUserToUser.java
@@ -21,26 +21,16 @@ import org.activiti.cloud.services.identity.keycloak.model.KeycloakUser;
 public class KeycloakUserToUser {
 
     public static User toUser(KeycloakUser kUser) {
-//        return User.builder()
-//            .id(kUser.getId())
-//            .firstName(kUser.getFirstName())
-//            .lastName(kUser.getLastName())
-//            .username(kUser.getUsername())
-//            .email(kUser.getEmail())
-//            .displayName(String.join(" ",
-//                kUser.getFirstName(),
-//                kUser.getLastName()))
-//            .build();
-
-
-        User user = new User();
-        user.setId(kUser.getId());
-        user.setUsername(kUser.getUsername());
-        user.setDisplayName(String.join(" ", kUser.getFirstName(), kUser.getLastName()));
-        user.setFirstName(kUser.getFirstName());
-        user.setLastName(kUser.getLastName());
-        user.setEmail(kUser.getEmail());
-        return user;
+        return User.builder()
+            .id(kUser.getId())
+            .firstName(kUser.getFirstName())
+            .lastName(kUser.getLastName())
+            .username(kUser.getUsername())
+            .email(kUser.getEmail())
+            .displayName(String.join(" ",
+                kUser.getFirstName(),
+                kUser.getLastName()))
+            .build();
     }
 
 }
