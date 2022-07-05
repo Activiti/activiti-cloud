61f08665146d0d2cbe8c557e181042bffbafd75a
Jakub Sokołowski <30300796+jsokolowskii@users.noreply.github.com>
2022-07-05 14:18:09 +0200

    allow same project name for different users (#747)

---
+++ activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-liquibase/src/main/resources/config/modeling/liquibase/changelog/04.pg.update.sql
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+alter table if exists project
+  drop constraint UK_3k75vvu7mevyvvb5may5lj8k7;
+alter table if exists project
+  add constraint unique_project_name_createdby unique (name, created_by);
