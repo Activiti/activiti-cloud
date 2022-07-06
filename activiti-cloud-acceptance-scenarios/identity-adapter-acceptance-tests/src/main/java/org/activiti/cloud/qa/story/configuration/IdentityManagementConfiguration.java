b0c47e4ba159b3cbea66ec6fe561fffd396ee2a9
Davide Cerbo <davide.cerbo@alfresco.com>
2022-07-06 17:34:15 +0200

    AAE-9036 New identity adapter service (#656)
    * AAE-9036 new identity adapter service
    
    * Update Dockerfile
    
    * refactor project and add a readme file
    
    * refactor test files
    
    * try new service and acceptance tests wip
    
    * add missing header
    
    * fix list indent
    
    * acceptance test wip
    
    * add acceptance tests and update version
    
    * add actuator and rename project
    
    * update identity service location
    
    * temporary change chart branch
    
    * update activiti-cloud version
    
    * add identity adapter
    
    * add identity adapter
    
    * update activiti-cloud version
    
    * add acceptance tests for identity
    
    * fix version
    
    * rename to configuration because config is in gitignore
    
    * fix pre commit
    
    * fix realm
    
    * fix realm
    
    * fix test avoiding using data from other tests
    
    * review markdonw
    
    * fix codconv issues
    
    * update to new preview version
    
    * restore entrypoint script
    
    * ignore codacy on script
    
    * update activiti clound and ran tests against modeling service
    
    * fix pom
    
    * fix permissions on community
    
    * fix permission for identity service
    
    * update version to latest develop version
    
    * Update Makefile

---
+++ activiti-cloud-acceptance-scenarios/identity-adapter-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/configuration/IdentityManagementConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.qa.story.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.jackson.JacksonEncoder;
+import org.activiti.cloud.acc.shared.rest.feign.EnableFeignContext;
+import org.activiti.cloud.acc.shared.rest.feign.FeignRestDataClient;
+import org.activiti.cloud.acc.shared.rest.feign.HalDecoder;
+import org.activiti.cloud.qa.story.client.IdentityManagementClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableFeignContext
+public class IdentityManagementConfiguration {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private IdentityManagementTestsConfigurationProperties properties;
+
+    @Bean
+    public IdentityManagementClient identityManagementClient(){
+        return FeignRestDataClient
+            .builder(new JacksonEncoder(objectMapper),
+                     new HalDecoder(objectMapper))
+            .contract(new SpringMvcContract())
+            .target(IdentityManagementClient.class,
+                    properties.getIdentityManagementUrl());
+    }
+
+}
