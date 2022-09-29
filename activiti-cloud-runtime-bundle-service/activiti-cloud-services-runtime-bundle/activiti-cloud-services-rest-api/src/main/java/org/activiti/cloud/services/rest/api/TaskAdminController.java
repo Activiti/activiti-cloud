fff7447288548f7b908c371ad337c4a9d8ee69c0
Jakub Sokołowski <30300796+jsokolowskii@users.noreply.github.com>
2022-09-29 16:18:44 +0200

    assign multiple tasks with one request

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-api/src/main/java/org/activiti/cloud/services/rest/api/TaskAdminController.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-api/src/main/java/org/activiti/cloud/services/rest/api/TaskAdminController.java
@@ -16,18 +16,22 @@
 package org.activiti.cloud.services.rest.api;
 
 import org.activiti.api.task.model.payloads.AssignTaskPayload;
+import org.activiti.api.task.model.payloads.AssignTasksPayload;
 import org.activiti.api.task.model.payloads.CompleteTaskPayload;
 import org.activiti.api.task.model.payloads.UpdateTaskPayload;
 import org.activiti.cloud.api.task.model.CloudTask;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping(value = "/admin/v1/tasks", produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
 public interface TaskAdminController {
@@ -52,4 +56,8 @@ public interface TaskAdminController {
     @RequestMapping(value = "/{taskId}/assign", method = RequestMethod.POST)
     EntityModel<CloudTask> assign(@PathVariable("taskId") String taskId,
                         @RequestBody AssignTaskPayload assignTaskPayload);
+
+    @PostMapping(value = "/assign", consumes = APPLICATION_JSON_VALUE)
+    PagedModel<EntityModel<CloudTask>> assign(@RequestBody AssignTasksPayload assignTasksPayload);
+
 }
