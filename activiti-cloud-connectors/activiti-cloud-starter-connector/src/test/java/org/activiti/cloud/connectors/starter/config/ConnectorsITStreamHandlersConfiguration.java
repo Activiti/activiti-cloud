9601b69fcefdbd60d2b2ef0c3d515b23989a2078
Lorenzo Di Giuseppe <lorenzo.digiuseppe@hyland.com>
2022-11-15 19:02:43 +0100

    WIP - rework connectors

---
+++ activiti-cloud-connectors/activiti-cloud-starter-connector/src/test/java/org/activiti/cloud/connectors/starter/config/ConnectorsITStreamHandlersConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.connectors.starter.config;
+
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import org.activiti.cloud.api.process.model.IntegrationError;
+import org.activiti.cloud.api.process.model.IntegrationResult;
+import org.activiti.cloud.common.messaging.functional.FunctionDefinition;
+import org.activiti.cloud.connectors.starter.test.it.ConnectorsITStreamHandlers;
+import org.activiti.cloud.connectors.starter.test.it.RuntimeMockStreams;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import reactor.core.publisher.Flux;
+
+@Profile(ConnectorsITStreamHandlers.CONNECTOR_IT)
+@Configuration
+public class ConnectorsITStreamHandlersConfiguration {
+
+    @FunctionDefinition(input = RuntimeMockStreams.INTEGRATION_RESULT_CONSUMER)
+    @Bean
+    public Consumer<Flux<IntegrationResult>> integrationMockResultConsumer(ConnectorsITStreamHandlers streamHandlers) {
+        return flux -> flux.log("integrationMockResultConsumer", Level.INFO).subscribe(result -> streamHandlers.consumeIntegrationResultsMock(result));
+    }
+
+    @FunctionDefinition(input = RuntimeMockStreams.INTEGRATION_ERROR_CONSUMER)
+    @Bean
+    public Consumer<Flux<IntegrationError>> integrationMockErrorConsumer(ConnectorsITStreamHandlers streamHandlers) {
+        return flux -> flux.log("integrationMockErrorConsumer", Level.INFO).subscribe(result -> streamHandlers.consumeIntegrationErrorMock(result));
+    }
+}
