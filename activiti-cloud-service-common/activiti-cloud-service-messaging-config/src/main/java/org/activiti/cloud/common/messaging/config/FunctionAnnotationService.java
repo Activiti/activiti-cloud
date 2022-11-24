76e407833cd8d845236c55ed672e70b8403c615b
Jakub Sokołowski <30300796+jsokolowskii@users.noreply.github.com>
2022-11-24 09:35:36 +0100

    WIP - fix activiti-cloud-messages-service bug with innerBean - refactoring

---
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionAnnotationService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.common.messaging.config;
+
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+import java.lang.annotation.Annotation;
+
+public class FunctionAnnotationService {
+
+    private static final Logger log = LoggerFactory.getLogger(FunctionBindingConfiguration.class);
+
+    private DefaultListableBeanFactory beanFactory;
+
+    public FunctionAnnotationService(DefaultListableBeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    @Nullable
+    public <T extends Annotation> T findAnnotationOnBean(String beanName, Class<T> annotationType) {
+        try {
+            return beanFactory.findAnnotationOnBean(beanName, annotationType);
+        } catch (NoSuchBeanDefinitionException e) {
+            log.warn("Bean with name {} not found.", beanName);
+            return null;
+        }
+    }
+}
