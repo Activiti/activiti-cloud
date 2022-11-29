0e6eb56f77848be1df368ad8390a6dd0d2d21b25
gfertuso <giovanni.fertuso.uk@gmail.com>
2022-11-29 10:46:30 +0000

    Revert "Make project configuration mandatory (#848)"
    This reverts commit 952470c240e1ee6b50202ce1cc722beaf8493593.

--- activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-jpa/src/main/java/org/activiti/cloud/services/modeling/entity/ProjectConfigurationEntity.java
+++ activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-jpa/src/main/java/org/activiti/cloud/services/modeling/entity/ProjectConfigurationEntity.java
@@ -20,8 +20,6 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.util.Optional;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -40,12 +38,11 @@ public class ProjectConfigurationEntity implements ProjectConfiguration {
 
     @Id
     @JsonIgnore
-    @Column(name = "project_id")
     private String id;
 
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @OneToOne(fetch = FetchType.LAZY)
     @MapsId
-    @JoinColumn(name = "project_id")
+    @JoinColumn(name = "id")
     @JsonIgnore
     private ProjectEntity project;
 
@@ -55,12 +52,7 @@ public class ProjectConfigurationEntity implements ProjectConfiguration {
     }
 
     public ProjectConfigurationEntity(Boolean enableCandidateStarters) {
-        this.enableCandidateStarters = Optional.of(enableCandidateStarters).orElse(false);
-    }
-
-    public ProjectConfigurationEntity(Project project, Boolean enableCandidateStarters) {
-        this.project = (ProjectEntity) project;
-        this.enableCandidateStarters = Optional.ofNullable(enableCandidateStarters).orElse(false);
+        this.enableCandidateStarters = enableCandidateStarters;
     }
 
     public String getId() {
@@ -73,7 +65,7 @@ public class ProjectConfigurationEntity implements ProjectConfiguration {
 
     @Override
     public void setEnableCandidateStarters(Boolean enableCandidateStarters) {
-        this.enableCandidateStarters = Optional.ofNullable(enableCandidateStarters).orElse(false);
+        this.enableCandidateStarters = enableCandidateStarters;
     }
 
     @Override
--- activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-liquibase/src/main/resources/config/modeling/liquibase/changelog/05.pg.update.sql
+++ activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-liquibase/src/main/resources/config/modeling/liquibase/changelog/05.pg.update.sql
@@ -1,11 +1,7 @@
-CREATE TABLE project_configuration
+create table project_configuration
 (
-    project_id VARCHAR(255) NOT NULL,
-    enable_candidate_starters boolean NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (project_id),
-    FOREIGN KEY (project_id) REFERENCES project (id)
+    id varchar(255) not null,
+    enable_candidate_starters boolean,
+    primary key (id),
+    foreign key (id) references project (id)
 );
-
-INSERT INTO project_configuration (project_id, enable_candidate_starters)
-SELECT id, false
-FROM project;
