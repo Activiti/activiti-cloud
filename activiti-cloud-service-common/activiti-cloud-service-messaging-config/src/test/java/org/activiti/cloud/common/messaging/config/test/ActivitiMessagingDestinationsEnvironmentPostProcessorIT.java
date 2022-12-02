b901ba4e46d7be0531e14fc5cc2371858bb1d074
Lorenzo Di Giuseppe <lorenzo.digiuseppe@hyland.com>
2022-12-02 13:03:33 +0100

    WIP - fixed conditional definition config + added tests

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/test/java/org/activiti/cloud/common/messaging/config/test/ActivitiMessagingDestinationsEnvironmentPostProcessorIT.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/test/java/org/activiti/cloud/common/messaging/config/test/ActivitiMessagingDestinationsEnvironmentPostProcessorIT.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.activiti.cloud.common.messaging.config.test;
 
 import org.junit.jupiter.api.Test;
