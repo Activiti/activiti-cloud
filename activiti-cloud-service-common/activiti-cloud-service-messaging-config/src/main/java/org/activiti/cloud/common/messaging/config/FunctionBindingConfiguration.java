1af053932c41a0814ed7409d61e274a0283a9d3c
Lorenzo Di Giuseppe <lorenzo.digiuseppe@hyland.com>
2022-12-14 15:17:06 +0100

    cleanup

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionBindingConfiguration.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionBindingConfiguration.java
@@ -16,7 +16,6 @@
 package org.activiti.cloud.common.messaging.config;
 
 import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
