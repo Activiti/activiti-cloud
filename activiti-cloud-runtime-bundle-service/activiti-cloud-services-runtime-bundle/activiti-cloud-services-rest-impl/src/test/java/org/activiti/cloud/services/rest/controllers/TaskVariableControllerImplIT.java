fd3bebd38b153cf68d3151462a368b6926b8306a
Lorenzo Di Giuseppe <lorenzo.digiuseppe@hyland.com>
2022-12-19 17:21:22 +0100

    transaction management

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/TaskVariableControllerImplIT.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/TaskVariableControllerImplIT.java
@@ -28,6 +28,7 @@ import org.activiti.cloud.services.events.listeners.CloudProcessDeployedProducer
 import org.activiti.cloud.services.rest.assemblers.CollectionModelAssembler;
 import org.activiti.cloud.services.rest.assemblers.TaskVariableInstanceRepresentationModelAssembler;
 import org.activiti.cloud.services.rest.conf.ServicesRestWebMvcAutoConfiguration;
+import org.activiti.cloud.services.rest.config.StreamConfig;
 import org.activiti.common.util.conf.ActivitiCoreCommonUtilAutoConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.spring.process.conf.ProcessExtensionsAutoConfiguration;
@@ -66,7 +67,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         CloudEventsAutoConfiguration.class,
         ActivitiCoreCommonUtilAutoConfiguration.class,
         ProcessExtensionsAutoConfiguration.class,
-        ServicesRestWebMvcAutoConfiguration.class})
+        ServicesRestWebMvcAutoConfiguration.class,
+        StreamConfig.class})
 public class TaskVariableControllerImplIT {
 
     @Autowired
