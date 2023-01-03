c8429af0ce1f211fee9283aacb11936e0cd911fb
Lorenzo Di Giuseppe <lorenzo.digiuseppe@hyland.com>
2023-01-03 16:15:01 +0100

    fix style

--- activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-service/src/test/java/org/activiti/cloud/services/modeling/validation/process/BpmnModelServiceTaskCatchBoundaryValidatorTest.java
+++ activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-service/src/test/java/org/activiti/cloud/services/modeling/validation/process/BpmnModelServiceTaskCatchBoundaryValidatorTest.java
@@ -54,9 +54,8 @@ public class BpmnModelServiceTaskCatchBoundaryValidatorTest {
     private FlowElementsExtractor flowElementsExtractor;
 
     @BeforeEach
-    public void setUp(){
-        validator = new BpmnModelServiceTaskCatchBoundaryValidator(flowElementsExtractor,
-            Arrays.asList(ServiceTaskImplementationType.SCRIPT_TASK));
+    public void setUp() {
+        validator = new BpmnModelServiceTaskCatchBoundaryValidator(flowElementsExtractor, Arrays.asList(ServiceTaskImplementationType.SCRIPT_TASK));
     }
 
     @Test
