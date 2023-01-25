1e5eb648d3314d3f39926a513f808794aea00dd4
Lorenzo Di Giuseppe <114941828+LorenzoDiGiuseppe@users.noreply.github.com>
2023-01-25 12:58:52 +0100

    Change set Process Variables API to PUT  (#963)
    * align methods for process instance variables update
    
    * fix integration test
    
    * fix acceptance test service
    
    * change setVariables to updateVariables in AT

--- activiti-cloud-acceptance-tests/activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/services/runtime/ProcessVariablesRuntimeService.java
+++ activiti-cloud-acceptance-tests/activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/services/runtime/ProcessVariablesRuntimeService.java
@@ -29,9 +29,9 @@ public interface ProcessVariablesRuntimeService {
     @Headers("Accept: application/hal+json;charset=UTF-8")
     CollectionModel<CloudVariableInstance> getVariables(@Param("id") String id);
 
-    @RequestLine("POST /v1/process-instances/{id}/variables")
+    @RequestLine("PUT /v1/process-instances/{id}/variables")
     @Headers("Content-Type: application/json")
-    ResponseEntity<Void> setVariables(@Param("id") String id,
+    ResponseEntity<Void> updateVariables(@Param("id") String id,
                                       SetProcessVariablesPayload setProcessVariablesPayload);
 
 }
