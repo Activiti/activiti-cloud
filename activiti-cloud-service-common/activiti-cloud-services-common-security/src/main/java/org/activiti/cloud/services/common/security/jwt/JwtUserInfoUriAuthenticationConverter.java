ef11f1e2b8be898d86bf1a9727cc588b7e29460f
Salvatore Caccia <salvatore.caccia@hyland.com>
2023-02-13 15:03:07 +0100

    [AAE-12550] - add sync cache to loadUserInfo API (#1004)
    * AAE-12550: add sync cache to loadUserInfo API
    
    * AAE-12550: remove unused log definition
    
    * AAE-12550: remove unused import

--- activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/services/common/security/jwt/JwtUserInfoUriAuthenticationConverter.java
+++ activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/services/common/security/jwt/JwtUserInfoUriAuthenticationConverter.java
@@ -22,7 +22,6 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AccessToken.TokenType;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -33,15 +32,15 @@ public class JwtUserInfoUriAuthenticationConverter implements Converter<Jwt, Abs
 
     private final Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter;
     private ClientRegistration clientRegistration;
-    private OAuth2UserService oAuth2UserService;
+    private OAuth2UserServiceCacheable oAuth2UserServiceCacheable;
     private String usernameClaim = "preferred_username";
 
     public JwtUserInfoUriAuthenticationConverter(Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter,
                                                  ClientRegistration clientRegistration,
-                                                 OAuth2UserService oAuth2UserService) {
+                                                 OAuth2UserServiceCacheable oAuth2UserServiceCacheable) {
         this.jwtGrantedAuthoritiesConverter = jwtGrantedAuthoritiesConverter;
         this.clientRegistration = clientRegistration;
-        this.oAuth2UserService = oAuth2UserService;
+        this.oAuth2UserServiceCacheable = oAuth2UserServiceCacheable;
     }
 
     @Override
@@ -62,7 +61,7 @@ public class JwtUserInfoUriAuthenticationConverter implements Converter<Jwt, Abs
             Instant expiresAt = jwt.getExpiresAt();
             OAuth2AccessToken accessToken = new OAuth2AccessToken(TokenType.BEARER, jwt.getTokenValue(), issuedAt, expiresAt);
             OAuth2UserRequest userRequest = new OAuth2UserRequest(clientRegistration, accessToken);
-            OAuth2User oAuth2User = this.oAuth2UserService.loadUser(userRequest);
+            OAuth2User oAuth2User = this.oAuth2UserServiceCacheable.loadUser(userRequest);
             username = oAuth2User.getName();
         }
         return username;
---
+++ activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/services/common/security/jwt/OAuth2UserServiceCacheable.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.services.common.security.jwt;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class OAuth2UserServiceCacheable {
+
+    private final OAuth2UserService oAuth2UserService;
+
+    public OAuth2UserServiceCacheable(OAuth2UserService oAuth2UserService) {
+        this.oAuth2UserService = oAuth2UserService;
+    }
+
+    @Cacheable(value = "userInfoApiCall", key = "#userRequest.accessToken.tokenValue", sync = true)
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        return  oAuth2UserService.loadUser(userRequest);
+    }
+}
