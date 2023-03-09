bb7d0a68b460a40f12408f13f987acbb81e620c5
Salvatore Caccia <salvatore.caccia@hyland.com>
2023-03-09 17:45:57 +0100

    Activiti/Activiti#4272: fix test and remove meaningless test

--- activiti-cloud-service-common/activiti-cloud-services-common-security/src/test/java/org/activiti/cloud/security/feign/ClientCredentialsAuthRequestInterceptorTest.java
+++ activiti-cloud-service-common/activiti-cloud-services-common-security/src/test/java/org/activiti/cloud/security/feign/ClientCredentialsAuthRequestInterceptorTest.java
@@ -49,7 +49,6 @@ class ClientCredentialsAuthRequestInterceptorTest {
 
     @Test
     public void shouldSetAuthorizationHeader_whenClientCredentials() {
-        when(clientRegistration.getAuthorizationGrantType()).thenReturn(AuthorizationGrantType.CLIENT_CREDENTIALS);
         when(clientRegistration.getRegistrationId()).thenReturn("keycloak");
         when(authorizedClientManager.authorize(any())).thenReturn(authorizedClient);
         when(authorizedClient.getAccessToken()).thenReturn(accessToken);
@@ -62,7 +61,6 @@ class ClientCredentialsAuthRequestInterceptorTest {
 
     @Test
     public void shouldSetOnlyOneAuthorizationHeader_whenClientCredentials() {
-        when(clientRegistration.getAuthorizationGrantType()).thenReturn(AuthorizationGrantType.CLIENT_CREDENTIALS);
         when(clientRegistration.getRegistrationId()).thenReturn("keycloak");
         when(authorizedClientManager.authorize(any())).thenReturn(authorizedClient);
         when(authorizedClient.getAccessToken()).thenReturn(accessToken);
@@ -74,15 +72,6 @@ class ClientCredentialsAuthRequestInterceptorTest {
         clientCredentialsAuthRequestInterceptor.apply(requestTemplate);
         assertThat(requestTemplate.headers()).hasSize(1);
         assertThat(requestTemplate.headers()).containsEntry("Authorization", List.of("Bearer 123"));
-    }
-
-    @Test
-    public void shouldNotSetAnyHeader_whenIsNotClientCredentials() {
-        when(clientRegistration.getAuthorizationGrantType()).thenReturn(AuthorizationGrantType.PASSWORD);
-
-        RequestTemplate requestTemplate = new RequestTemplate();
-        clientCredentialsAuthRequestInterceptor.apply(requestTemplate);
-        assertThat(requestTemplate.headers()).isEmpty();
     }
 
 }
