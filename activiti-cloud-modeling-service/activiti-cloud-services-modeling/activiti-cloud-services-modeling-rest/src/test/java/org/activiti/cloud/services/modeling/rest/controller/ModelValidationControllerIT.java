5dca1dfa9de702d13b4d70bf8922ea3fd124177f
Ishwar Gupta <guptaishwar1998@gmail.com>
2023-06-14 18:06:28 +0530

    AAE-7344 changing the integration test from 204 to 400

--- activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-rest/src/test/java/org/activiti/cloud/services/modeling/rest/controller/ModelValidationControllerIT.java
+++ activiti-cloud-modeling-service/activiti-cloud-services-modeling/activiti-cloud-services-modeling-rest/src/test/java/org/activiti/cloud/services/modeling/rest/controller/ModelValidationControllerIT.java
@@ -114,7 +114,7 @@ public class ModelValidationControllerIT {
 
         mockMvc
             .perform(multipart("/v1/models/{model_id}/validate", processModel.getId()).file(file))
-            .andExpect(status().isNoContent());
+            .andExpect(status().isBadRequest());
     }
 
     @Test
