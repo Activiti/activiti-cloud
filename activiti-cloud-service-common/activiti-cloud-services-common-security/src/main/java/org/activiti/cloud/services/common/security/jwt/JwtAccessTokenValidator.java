8c47d9a857b62c755d8ceb2b38824d08f87838d2
dcerbo <davidecerbo@gmail.com>
2023-07-04 10:25:06 +0200

    AAE-15549 add a log when a jwt token validation fails

--- activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/services/common/security/jwt/JwtAccessTokenValidator.java
+++ activiti-cloud-service-common/activiti-cloud-services-common-security/src/main/java/org/activiti/cloud/services/common/security/jwt/JwtAccessTokenValidator.java
@@ -18,10 +18,14 @@ package org.activiti.cloud.services.common.security.jwt;
 import java.util.List;
 import java.util.Optional;
 import org.activiti.cloud.services.common.security.jwt.validator.ValidationCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 public class JwtAccessTokenValidator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAccessTokenValidator.class);
 
     private final List<ValidationCheck> validationChecks;
 
@@ -38,6 +42,15 @@ public class JwtAccessTokenValidator {
     }
 
     public boolean isValid(Jwt accessToken) {
-        return !validationChecks.stream().map(check -> check.isValid(accessToken)).anyMatch(b -> b.equals(false));
+        return !validationChecks
+            .stream()
+            .map(check -> {
+                boolean valid = check.isValid(accessToken);
+                if(!valid) {
+                    LOGGER.error("Token invalid because the {} validation has been failed.", check.getClass().toString());
+                }
+                return valid;
+            })
+            .anyMatch(b -> b.equals(false));
     }
 }
