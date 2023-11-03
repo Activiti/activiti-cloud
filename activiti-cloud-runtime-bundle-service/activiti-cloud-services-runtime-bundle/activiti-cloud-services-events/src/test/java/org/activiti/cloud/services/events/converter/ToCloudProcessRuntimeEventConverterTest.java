34ea45410ed25319f03630857f95bf46180168a1
Riccardo Grandi <riccardo.grandi@hyland.com>
2023-11-03 15:02:21 +0100

    [AAE-17650] - Fix codacy

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/converter/ToCloudProcessRuntimeEventConverterTest.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/converter/ToCloudProcessRuntimeEventConverterTest.java
@@ -19,7 +19,6 @@ package org.activiti.cloud.services.events.converter;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
