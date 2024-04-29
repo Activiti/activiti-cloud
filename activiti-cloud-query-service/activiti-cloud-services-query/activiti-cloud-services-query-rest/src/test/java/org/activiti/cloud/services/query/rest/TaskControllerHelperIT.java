2a1d1e3bf856fbca4a0c79e1dfc0f021cb529b5f
jesty <davidecerbo@gmail.com>
2024-04-29 15:29:35 +0200

    WIP

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/TaskControllerHelperIT.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/TaskControllerHelperIT.java
@@ -1,14 +1,13 @@
 package org.activiti.cloud.services.query.rest;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.querydsl.core.types.Predicate;
-import java.util.Collections;
-import java.util.List;
+import jakarta.persistence.EntityManager;
 import org.activiti.cloud.api.task.model.QueryCloudTask;
+import org.activiti.cloud.services.query.app.repository.ProcessInstanceRepository;
 import org.activiti.cloud.services.query.app.repository.TaskRepository;
 import org.activiti.cloud.services.query.app.repository.TaskVariableRepository;
+import org.activiti.cloud.services.query.model.ProcessInstanceEntity;
+import org.activiti.cloud.services.query.model.ProcessVariableEntity;
 import org.activiti.cloud.services.query.model.TaskEntity;
 import org.activiti.cloud.services.query.model.TaskVariableEntity;
 import org.activiti.cloud.services.query.rest.predicate.QueryDslPredicateFilter;
@@ -20,6 +19,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @SpringBootTest(properties = "spring.main.banner-mode=off")
 @TestPropertySource("classpath:application-test.properties")
@@ -34,20 +38,43 @@ public class TaskControllerHelperIT {
 
     @Autowired
     TaskVariableRepository taskVariableRepository;
+    @Autowired
+    private ProcessInstanceRepository processInstanceRepository;
+    @Autowired
+    private org.activiti.cloud.services.query.app.repository.VariableRepository variableRepository;
+
+    @Autowired
+    EntityManager entityManager;
 
     @Test
+    @Transactional
     public void shouldtest() {
+        ProcessInstanceEntity processInstanceEntity = new ProcessInstanceEntity();
+        processInstanceEntity.setId("15");
+        processInstanceEntity.setName("name");
+        processInstanceEntity.setInitiator("initiator");
+        processInstanceEntity.setProcessDefinitionName("test");
+        processInstanceEntity.setProcessDefinitionKey("defKey1");
+        processInstanceEntity.setServiceName("test-cmd-endpoint");
+        processInstanceRepository.save(processInstanceEntity);
+
+        ProcessVariableEntity processVariableEntity = new ProcessVariableEntity();
+        processVariableEntity.setName("name");
+        processVariableEntity.setValue("id");
+        processVariableEntity.setProcessInstanceId("15");
+        processVariableEntity.setProcessInstance(processInstanceEntity);
+        variableRepository.save(processVariableEntity);
+
         TaskEntity taskEntity = new TaskEntity();
         taskEntity.setId("1");
+        taskEntity.setProcessVariables(Set.of(processVariableEntity));
+        taskEntity.setProcessInstance(processInstanceEntity);
+        taskEntity.setProcessInstanceId("15");
         taskRepository.save(taskEntity);
 
-        TaskVariableEntity variableEntity = new TaskVariableEntity();
-        variableEntity.setName("name");
-        variableEntity.setValue("id");
-        variableEntity.setTaskId("1");
-        variableEntity.setTask(taskEntity);
-        taskVariableRepository.save(variableEntity);
 
+        Set<ProcessVariableEntity> variables = processInstanceRepository.findById("15").get().getVariables();
+        //Long id = variables.iterator().next().getId();
         Predicate predicate = null;
         VariableSearch variableSearch = new VariableSearch(null, null, null);
         Pageable pageable = Pageable.ofSize(10);
@@ -66,5 +93,24 @@ public class TaskControllerHelperIT {
         EntityModel<QueryCloudTask> queryCloudTaskEntityModel = entityModels.get(0);
         assertThat(queryCloudTaskEntityModel.getContent().getProcessVariables().stream().toList().get(0).getName())
             .isEqualTo("name");
+    }
+
+    private ProcessVariableEntity buildVariable() {
+        ProcessVariableEntity variableEntity = new ProcessVariableEntity(
+            1L,
+            String.class.getName(),
+            "firstName",
+            UUID.randomUUID().toString(),
+            "My app",
+            "My app",
+            "1",
+            null,
+            null,
+            new Date(),
+            new Date(),
+            UUID.randomUUID().toString()
+        );
+        variableEntity.setValue("John");
+        return variableEntity;
     }
 }
