e7be3ad3afa96ddda17db9d341b0fa66b47ffd1b
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2024-04-30 13:04:47 +0200

    AAE-22075 reproduce exact situation of issue

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/TaskControllerHelperIT.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/TaskControllerHelperIT.java
@@ -1,10 +1,13 @@
 package org.activiti.cloud.services.query.rest;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.querydsl.core.types.Predicate;
-import jakarta.persistence.EntityManager;
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import org.activiti.cloud.api.task.model.QueryCloudTask;
 import org.activiti.cloud.services.query.app.repository.ProcessInstanceRepository;
 import org.activiti.cloud.services.query.app.repository.TaskRepository;
@@ -12,9 +15,7 @@ import org.activiti.cloud.services.query.app.repository.TaskVariableRepository;
 import org.activiti.cloud.services.query.model.ProcessInstanceEntity;
 import org.activiti.cloud.services.query.model.ProcessVariableEntity;
 import org.activiti.cloud.services.query.model.TaskEntity;
-import org.activiti.cloud.services.query.model.TaskVariableEntity;
 import org.activiti.cloud.services.query.rest.predicate.QueryDslPredicateFilter;
-import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -23,13 +24,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.transaction.TestTransaction;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(
     properties = {
         "spring.main.banner-mode=off",
-        "spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true",
+        "spring.jpa.properties.hibernate.enable_lazy_load_no_trans=false",
         "logging.level.org.hibernate.collection.spi=warn",
     }
 )
@@ -53,7 +52,6 @@ public class TaskControllerHelperIT {
     private org.activiti.cloud.services.query.app.repository.VariableRepository variableRepository;
 
     @Test
-    @Transactional
     public void shouldtest() {
         ProcessInstanceEntity processInstanceEntity = new ProcessInstanceEntity();
         processInstanceEntity.setId("15");
@@ -68,6 +66,7 @@ public class TaskControllerHelperIT {
         processVariableEntity.setName("name");
         processVariableEntity.setValue("id");
         processVariableEntity.setProcessInstanceId("15");
+        processVariableEntity.setProcessDefinitionKey("defKey1");
         processVariableEntity.setProcessInstance(processInstanceRepository.findById("15").orElseThrow());
         processInstanceEntity.setVariables(Set.of(processVariableEntity));
         variableRepository.save(processVariableEntity);
@@ -80,18 +79,16 @@ public class TaskControllerHelperIT {
         taskEntity.setProcessInstanceId("15");
         taskRepository.save(taskEntity);
 
-        TestTransaction.flagForCommit();
-        TestTransaction.end();
-
         Set<ProcessVariableEntity> variables = processInstanceRepository.findById("15").get().getVariables();
 
         assertThat(variables).isNotEmpty();
-        // Long id = variables.iterator().next().getId();
+
         Predicate predicate = null;
         VariableSearch variableSearch = new VariableSearch(null, null, null);
         Pageable pageable = Pageable.ofSize(10);
         List<QueryDslPredicateFilter> filters = Collections.emptyList();
-        List<String> processVariableKeys = List.of("name");
+        List<String> processVariableKeys = List.of("defKey1/name");
+
         PagedModel<EntityModel<QueryCloudTask>> allWithProcessVariables = taskControllerHelper.findAllWithProcessVariables(
             predicate,
             variableSearch,
