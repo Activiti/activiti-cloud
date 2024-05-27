3e93aaf3291534b3ff2f40c837eaa9c643593e02
Igor Dianov <igor@dianov.com>
2024-05-26 21:27:37 -0700

    Add integration tests

--- activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
+++ activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
@@ -20,3 +20,7 @@ insert into TASK_VARIABLE (id, create_time, execution_id, last_updated_time, nam
   (5, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable4', 0, '2', 'map', '{"value": { "key" : "data" }}'),
   (6, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable5', 1, '4', 'String', '{"value": 1.0}'),
   (7, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable6', 1, '4', 'list', '{"value": [1,2,3,4,5]}');
+
+insert into TASK_PROCESS_VARIABLE (task_id, process_variable_id) values
+  (4,1),
+  (5,1);
