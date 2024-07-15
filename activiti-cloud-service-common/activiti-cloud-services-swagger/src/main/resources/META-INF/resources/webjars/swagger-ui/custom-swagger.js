d15fbde2cb1a0cb4dd5b33e2ad92808d2483d4f7
Federico D'Ulizia <federico.dulizia@hyland.com>
2024-07-15 14:28:47 +0200

    AAE-23742 fix keykcloak swagger configuration and change custom-swagger.js

--- activiti-cloud-service-common/activiti-cloud-services-swagger/src/main/resources/META-INF/resources/webjars/swagger-ui/custom-swagger.js
+++ activiti-cloud-service-common/activiti-cloud-services-swagger/src/main/resources/META-INF/resources/webjars/swagger-ui/custom-swagger.js
@@ -23,7 +23,7 @@ function tryRefreshOauth2Token() {
     return;
   }
 
-  const {schema, clientId} = auth;
+  const {schema, clientId, token} = auth;
   const errors = [];
 
   if (schema == null) {
@@ -35,112 +35,36 @@ function tryRefreshOauth2Token() {
   if (clientId == null) {
     errors.push('Invalid auth: missing clientId');
   }
+
+  if (token == null) {
+    errors.push('Invalid auth: missing token');
+  }
+  if (token?.refresh_token == null) {
+    errors.push('Invalid auth: missing refresh token');
+  }
+  if (token?.scope == null) {
+    errors.push('Invalid auth: missing scope');
+  }
   if (errors.length) {
     console.log("Can't refresh token due to the following issues:");
     errors.forEach(console.log);
     return;
   }
 
-  let refreshToken = null;
-  let refreshTokenKey = null;
-  let scope = null;
-  let scopeKey = null;
-  const result = findTokenAndScope();
-  refreshToken = result.refreshTokenValue;
-  refreshTokenKey = result.refreshTokenKey;
-
-  scope = result.scopeValue;
-  scopeKey = result.scopeKey;
-
-  if (refreshToken && scope) {
-    console.log('Refresh token:', refreshToken);
-    console.log('Scope:', scope);
-  } else {
-    console.log('Refresh token or scope not found.');
-  }
-
   const form = {
     grant_type: 'refresh_token',
-    refresh_token: refreshToken,
+    refresh_token: token.refresh_token,
     client_id: clientId,
-    scope: scope,
-    audience: undefined,
+    scope: token.scope,
   };
 
   console.log(`Refreshing token...`);
-
-  fetch(schema.tokenUrl, {
-    method: 'POST',
-    headers: {
-      'accept': 'application/json, text/plain, */*',
-      'content-type': 'application/x-www-form-urlencoded',
-    },
-    body: buildFormData(form)
-  })
-    .then(response => response.json())
-    .then(data => {
-
-      if (data) {
-        const refreshToken = data.refresh_token;
-        const scope = data.scope;
-
-        if (refreshToken) {
-          localStorage.setItem(refreshTokenKey, refreshToken);
-          console.log('Refresh token:', refreshToken);
-        }
-
-        if (scope) {
-          localStorage.setItem(scopeKey, scope);
-          console.log('Scope:', scope);
-        }
-      } else {
-        console.log('No data in response:', data);
-      }
-
-      console.log('Access token refreshed:', data.refresh_token);
-
-      const token = data;
-
-      const ui = window.ui;
-      if (ui?.authActions?.authorizeOauth2WithPersistOption && token) {
-        ui.authActions.authorizeOauth2WithPersistOption({auth, token})
-      } else {
-        console.error('Swagger UI authActions not available.');
-      }
-    })
-    .catch(error => console.error('Error refreshing token:', error));
-}
-
-function findTokenAndScope() {
-  let refreshTokenKey = null;
-  let refreshTokenValue = null;
-  let scopeKey = null;
-  let scopeValue = null;
-
-
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-
-    if (key?.includes('refresh_token')) {
-      refreshTokenKey = key;
-      refreshTokenValue = localStorage.getItem(key);
-    } else if (key?.includes('granted_scopes')) {
-      scopeKey = key;
-      scopeValue = localStorage.getItem(key);
-    }
-
-    // Break the loop early if both values are found
-    if (scopeValue && refreshTokenValue) {
-      break;
-    }
-  }
-
-  return {
-    refreshTokenValue: refreshTokenValue,
-    refreshTokenKey: refreshTokenKey,
-    scopeValue: scopeValue,
-    scopeKey: scopeKey
-  };
+  window.ui.authActions.authorizeRequest({
+    body: buildFormData(form),
+    name,
+    url: schema.tokenUrl,
+    auth,
+  });
 }
 
 function startClock() {
@@ -184,7 +108,6 @@ function patchRefreshHook() {
     // If the token can expire, schedule a token refresh and update the timer
     if (payload.token.expires_in) {
       const tokenRefreshTimeout = payload.token.expires_in * 750;
-      const tokenExpiryTimeout = payload.token.expires_in;
       console.log(
         `Refreshable token detected. Scheduling token refresh in ${(
           tokenRefreshTimeout /
@@ -195,7 +118,6 @@ function patchRefreshHook() {
         )}min)...`,
       );
       window.tokenRefreshTime = Date.now() + payload.token.expires_in * 750;
-      window.tokenExpiryTime = Date.now() + payload.token.expires_in * 1000;
 
       // Start the clock
       startClock();
