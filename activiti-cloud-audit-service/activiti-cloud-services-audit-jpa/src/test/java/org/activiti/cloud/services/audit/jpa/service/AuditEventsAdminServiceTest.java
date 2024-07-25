0a0294c389168366b89e0616b766972beca79b44
RahulKumar1606 <rahul.kumar@hyland.com>
2024-07-25 11:07:51 +0530

    AAE-24320 Remove unused import

--- activiti-cloud-audit-service/activiti-cloud-services-audit-jpa/src/test/java/org/activiti/cloud/services/audit/jpa/service/AuditEventsAdminServiceTest.java
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-jpa/src/test/java/org/activiti/cloud/services/audit/jpa/service/AuditEventsAdminServiceTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @ExtendWith(MockitoExtension.class)
 class AuditEventsAdminServiceTest {
