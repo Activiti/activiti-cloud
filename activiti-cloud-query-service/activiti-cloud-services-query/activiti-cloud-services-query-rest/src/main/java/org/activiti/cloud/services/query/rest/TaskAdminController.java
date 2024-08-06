609f33da96760c94023cd62f9a61059e5a37ed98
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2024-08-06 14:30:20 +0200

    AAE-24331 fix sonarcloud issues

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/TaskAdminController.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/TaskAdminController.java
@@ -51,6 +51,7 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -142,7 +143,7 @@ public class TaskAdminController {
 
     @Operation(summary = "Search tasks", hidden = true)
     @JsonView(JsonViews.General.class)
-    @RequestMapping(method = RequestMethod.POST, value = "/search")
+    @PostMapping("/search")
     public PagedModel<EntityModel<QueryCloudTask>> searchTasks(
         @RequestBody TaskSearchRequest taskSearchRequest,
         Pageable pageable
