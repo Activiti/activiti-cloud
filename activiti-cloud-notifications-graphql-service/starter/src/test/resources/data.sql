9fecb2bc98f6097684f9abb8aee6e85786984f65
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2024-08-07 12:00:14 +0200

    AAE-22454 fix graphql tests

--- activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
+++ activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
@@ -52,15 +52,15 @@ insert into task (id, assignee, business_key, created_date, description, due_dat
   ('6', 'assignee', 'bk6', CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task6', 10, 'process_definition_id', 0, 'ASSIGNED'  , 'owner', null);
 
 insert into PROCESS_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, type, "value") values
-  (1, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'initiator', 1, 'map', '{"value": { "key" : ["1","2","3","4","5"]}}');
+  (1, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'initiator', 1, 'map', JSON '{"value": { "key" : ["1","2","3","4","5"]}}');
 
 insert into TASK_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, task_id, type, "value") values
-  (2, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable1', 0, '1', 'String', '{"value": "10"}'),
-  (3, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable2', 0, '1', 'String', '{"value": true}'),
-  (4, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable3', 0, '2', 'String', '{"value": null}'),
-  (5, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable4', 0, '2', 'map', '{"value": { "key" : "data" }}'),
-  (6, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable5', 1, '4', 'String', '{"value": 1.0}'),
-  (7, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable6', 1, '4', 'list', '{"value": [1,2,3,4,5]}');
+  (2, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable1', 0, '1', 'String', JSON '{"value": "10"}'),
+  (3, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable2', 0, '1', 'String', JSON '{"value": true}'),
+  (4, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable3', 0, '2', 'String', JSON '{"value": null}'),
+  (5, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable4', 0, '2', 'map', JSON '{"value": { "key" : "data" }}'),
+  (6, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable5', 1, '4', 'String', JSON '{"value": 1.0}'),
+  (7, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable6', 1, '4', 'list', JSON '{"value": [1,2,3,4,5]}');
 
 insert into TASK_PROCESS_VARIABLE (task_id, process_variable_id) values
   (4,1),
