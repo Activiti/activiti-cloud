7e51ec3a4f490473f4bfc4ad38e4867f40c61978
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2024-08-08 15:24:30 +0200

    AAE-22454 codacy issues

--- activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
+++ activiti-cloud-notifications-graphql-service/starter/src/test/resources/data.sql
@@ -1,49 +1,49 @@
-create table if not exists PROCESS_VARIABLE (
-  marked_as_deleted boolean,
-  create_time timestamp(6),
-  id bigint not null,
-  last_updated_time timestamp(6),
-  app_name varchar(255),
-  app_version varchar(255),
-  execution_id varchar(255),
-  name varchar(255),
-  process_definition_key varchar(255),
-  process_instance_id varchar(255),
-  service_full_name varchar(255),
-  service_name varchar(255),
-  service_type varchar(255),
-  service_version varchar(255),
-  type varchar(255),
-  variable_definition_id varchar(255),
-  "value" json,
-  primary key (id)
+CREATE TABLE IF NOT EXISTS PROCESS_VARIABLE (
+  marked_as_deleted BOOLEAN,
+  create_time TIMESTAMP(6),
+  id BIGINT NOT NULL,
+  last_updated_time TIMESTAMP(6),
+  app_name VARCHAR(255),
+  app_version VARCHAR(255),
+  execution_id VARCHAR(255),
+  name VARCHAR(255),
+  process_definition_key VARCHAR(255),
+  process_instance_id VARCHAR(255),
+  service_full_name VARCHAR(255),
+  service_name VARCHAR(255),
+  service_type VARCHAR(255),
+  service_version VARCHAR(255),
+  type VARCHAR(255),
+  variable_definition_id VARCHAR(255),
+  "value" JSON,
+  PRIMARY KEY (id)
 );
 
-create table if not exists TASK_VARIABLE (
-  marked_as_deleted boolean,
-  create_time timestamp(6),
-  id bigint not null,
-  last_updated_time timestamp(6),
-  app_name varchar(255),
-  app_version varchar(255),
-  execution_id varchar(255),
-  name varchar(255),
-  process_instance_id varchar(255),
-  service_full_name varchar(255),
-  service_name varchar(255),
-  service_type varchar(255),
-  service_version varchar(255),
-  task_id varchar(255),
-  type varchar(255),
-  "value" json,
-  primary key (id)
+CREATE TABLE IF NOT EXISTS TASK_VARIABLE (
+  marked_as_deleted BOOLEAN,
+  create_time TIMESTAMP(6),
+  id BIGINT NOT NULL,
+  last_updated_time TIMESTAMP(6),
+  app_name VARCHAR(255),
+  app_version VARCHAR(255),
+  execution_id VARCHAR(255),
+  name VARCHAR(255),
+  process_instance_id VARCHAR(255),
+  service_full_name VARCHAR(255),
+  service_name VARCHAR(255),
+  service_type VARCHAR(255),
+  service_version VARCHAR(255),
+  task_id VARCHAR(255),
+  type VARCHAR(255),
+  "value" JSON,
+  PRIMARY KEY (id)
 );
 
-insert into process_instance (id, last_modified, last_modified_from, last_modified_to, process_definition_id, status, initiator, start_date, business_key, process_definition_key) values
+INSERT INTO process_instance (id, last_modified, last_modified_from, last_modified_to, process_definition_id, status, initiator, start_date, business_key, process_definition_key) VALUES
   ('0', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key'),
   ('1', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key');
 
-insert into task (id, assignee, business_key, created_date, description, due_date, last_modified, last_modified_from, last_modified_to, name, priority, process_definition_id, process_instance_id, status, owner, claimed_date) values
+INSERT INTO task (id, assignee, business_key, created_date, description, due_date, last_modified, last_modified_from, last_modified_to, name, priority, process_definition_id, process_instance_id, status, owner, claimed_date) VALUES
   ('1', 'assignee', 'bk1', CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task1', 5, 'process_definition_id', 0, 'COMPLETED'  , 'owner', null),
   ('2', 'assignee', null, CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task2', 10, 'process_definition_id', 0, 'CREATED'  , 'owner', null),
   ('3', 'assignee', null, CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task3', 5, 'process_definition_id', 0, 'CREATED'  , 'owner', null),
@@ -51,10 +51,10 @@ insert into task (id, assignee, business_key, created_date, description, due_dat
   ('5', 'assignee', null, CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task5', 10, 'process_definition_id', 1, 'COMPLETED'  , 'owner', null),
   ('6', 'assignee', 'bk6', CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task6', 10, 'process_definition_id', 0, 'ASSIGNED'  , 'owner', null);
 
-insert into PROCESS_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, type, "value") values
+INSERT INTO PROCESS_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, type, "value") VALUES
   (1, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'initiator', 1, 'map', JSON '{"value": { "key" : ["1","2","3","4","5"]}}');
 
-insert into TASK_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, task_id, type, "value") values
+INSERT INTO TASK_VARIABLE (id, create_time, execution_id, last_updated_time, name, process_instance_id, task_id, type, "value") VALUES
   (2, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable1', 0, '1', 'String', JSON '{"value": "10"}'),
   (3, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable2', 0, '1', 'String', JSON '{"value": true}'),
   (4, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable3', 0, '2', 'String', JSON '{"value": null}'),
@@ -62,6 +62,6 @@ insert into TASK_VARIABLE (id, create_time, execution_id, last_updated_time, nam
   (6, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable5', 1, '4', 'String', JSON '{"value": 1.0}'),
   (7, CURRENT_TIMESTAMP, 'execution_id', CURRENT_TIMESTAMP, 'variable6', 1, '4', 'list', JSON '{"value": [1,2,3,4,5]}');
 
-insert into TASK_PROCESS_VARIABLE (task_id, process_variable_id) values
+INSERT INTO TASK_PROCESS_VARIABLE (task_id, process_variable_id) VALUES
   (4,1),
   (5,1);
