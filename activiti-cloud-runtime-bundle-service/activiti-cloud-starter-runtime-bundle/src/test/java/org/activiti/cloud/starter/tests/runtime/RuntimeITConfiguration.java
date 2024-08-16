8d6e7a1f1c69035bc71aed9f4e775358907ca4f4
Wojciech Piotrowiak <wojciech.piotrowiak@hyland.com>
2024-08-16 17:35:40 +0200

    AAE-23395 reverts

--- activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/RuntimeITConfiguration.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/RuntimeITConfiguration.java
@@ -17,9 +17,7 @@ package org.activiti.cloud.starter.tests.runtime;
 
 import org.activiti.cloud.starter.tests.helper.HelperConfiguration;
 import org.activiti.cloud.starter.tests.util.VariablesUtil;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 @TestConfiguration
@@ -33,5 +31,4 @@ import org.springframework.context.annotation.Import;
         VariablesUtil.class,
     }
 )
-public class RuntimeITConfiguration {
-}
+public class RuntimeITConfiguration {}
