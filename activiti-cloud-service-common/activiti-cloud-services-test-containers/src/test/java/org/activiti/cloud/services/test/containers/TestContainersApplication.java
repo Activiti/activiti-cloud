330fee3d4683cbff4f3c0f648895cc895962366a
Jakub Sokołowski <30300796+jsokolowskii@users.noreply.github.com>
2024-09-13 12:05:00 +0200

    AAE-23175 Upgrade testcontainers keycloak to 3.4.0

--- activiti-cloud-service-common/activiti-cloud-services-test-containers/src/test/java/org/activiti/cloud/services/test/containers/TestContainersApplication.java
+++ activiti-cloud-service-common/activiti-cloud-services-test-containers/src/test/java/org/activiti/cloud/services/test/containers/TestContainersApplication.java
@@ -19,9 +19,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakApplication {
+public class TestContainersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KeycloakApplication.class, args);
+        SpringApplication.run(TestContainersApplication.class, args);
     }
 }
