8eadc2aee1ec8a5bae0a196bd47dab6ece8eecf6
Igor Dianov <igor@dianov.com>
2024-09-16 06:12:55 -0700

    AAE-25756 Add Query service schema index for task id,name and status attributes + Refactored to resolve conflicts

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/h2.schema.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/h2.schema.sql
@@ -312,5 +312,6 @@ alter table task_process_variable
 create index idx_task_assignee on task(assignee);
 create index idx_task_owner on task(owner);
 create index idx_process_instance_initiator on process_instance(initiator);
+CREATE INDEX idx_task_id_name_status ON task(id, name, status);
 CREATE INDEX idx_task_process_var_taskId_processVarId ON task_process_variable (task_id, process_variable_id);
 CREATE INDEX idx_task_createdDate ON task (created_date);
