6c7dfa75da6b58c5df290101b052704d173402f5
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2024-10-23 16:36:40 +0200

    AAE-25188 Sort tasks by process variables (#1574)
    * AAE-25188 refactoring
    
    * AAE-25188 refactoring
    
    * AAE-25188 refactoring
    
    * AAE-25188 refactoring
    
    * AAE-25188 apply sorting to TaskSpecification, fix other tests
    
    * AAE-25189 rebase
    
    * AAE-25189 fix QuerySwaggerIT

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/util/TaskSearchRequestBuilder.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/util/TaskSearchRequestBuilder.java
@@ -18,11 +18,13 @@ package org.activiti.cloud.services.query.util;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.activiti.api.task.model.Task;
 import org.activiti.cloud.services.query.model.ProcessVariableKey;
 import org.activiti.cloud.services.query.rest.filter.VariableFilter;
+import org.activiti.cloud.services.query.rest.payload.CloudRuntimeEntitySort;
 import org.activiti.cloud.services.query.rest.payload.TaskSearchRequest;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
@@ -53,6 +55,7 @@ public class TaskSearchRequestBuilder {
     private Set<VariableFilter> taskVariableFilters;
     private Set<VariableFilter> processVariableFilters;
     private Set<ProcessVariableKey> processVariableKeys;
+    private CloudRuntimeEntitySort sort;
 
     public TaskSearchRequestBuilder onlyStandalone() {
         this.onlyStandalone = true;
@@ -174,6 +177,11 @@ public class TaskSearchRequestBuilder {
         return this;
     }
 
+    public TaskSearchRequestBuilder withSort(CloudRuntimeEntitySort sort) {
+        this.sort = sort;
+        return this;
+    }
+
     public TaskSearchRequest build() {
         if (processVariableFilters != null) {
             Set<ProcessVariableKey> keysFromFilters = processVariableFilters
@@ -185,6 +193,7 @@ public class TaskSearchRequestBuilder {
             if (processVariableKeys == null) {
                 processVariableKeys = keysFromFilters;
             } else {
+                processVariableKeys = new HashSet<>(processVariableKeys);
                 processVariableKeys.addAll(keysFromFilters);
             }
         }
@@ -212,7 +221,8 @@ public class TaskSearchRequestBuilder {
             candidateGroupId,
             taskVariableFilters,
             processVariableFilters,
-            processVariableKeys
+            processVariableKeys,
+            sort
         );
     }
 
