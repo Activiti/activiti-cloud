917453c1f08d4b958fd1c8d1c60f679e5740dae4
Wojciech Piotrowiak <wojciech.piotrowiak@hyland.com>
2025-02-12 10:22:07 +0100

    AAE-30611 format

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/ProcessInstanceEntitySearchAdminControllerIT.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/ProcessInstanceEntitySearchAdminControllerIT.java
@@ -38,7 +38,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
     properties = {
         "spring.main.banner-mode=off",
         "spring.jpa.properties.hibernate.enable_lazy_load_no_trans=false",
-        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect"
+        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
     }
 )
 @TestPropertySource("classpath:application-test.properties")
