a37128f3ab91c0a557544516d3baad59d63eee4c
revatilimaye <revati.limaye@hyland.com>
2025-03-03 18:33:24 +0530

    AAE-31941 revert wrongly done change

--- activiti-cloud-notifications-graphql-service/services/jpa-query/src/test/resources/data.sql
+++ activiti-cloud-notifications-graphql-service/services/jpa-query/src/test/resources/data.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS PROCESS_VARIABLE (
   type VARCHAR(255),
   variable_definition_id VARCHAR(255),
   "value" JSON,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
 );
 
 CREATE TABLE IF NOT EXISTS TASK_VARIABLE (
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS TASK_VARIABLE (
 );
 
 INSERT INTO process_instance (id, last_modified, last_modified_from, last_modified_to, process_definition_id, status, initiator, start_date, business_key, process_definition_key) VALUES
-  ('0', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key',false),
-  ('1', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key',false);
+  ('0', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key'),
+  ('1', CURRENT_TIMESTAMP, null, null, 'process_definition_id', 'RUNNING', 'initiator',null, 'bus_key','def_key');
 
 INSERT INTO task (id, assignee, created_date, description, due_date, last_modified, last_modified_from, last_modified_to, name, priority, process_definition_id, process_instance_id, status, owner, claimed_date) VALUES
   ('1', 'assignee', CURRENT_TIMESTAMP, 'description', null, null, null, null, 'task1', 5, 'process_definition_id', 0, 'COMPLETED'  , 'owner', null),
