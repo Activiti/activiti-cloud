c650498fb990cdfb316ba44a92f75b13f930e7f1
revatilimaye <revati.limaye@hyland.com>
2025-03-11 13:44:36 +0530

    AAE-31941 remove unused import

--- activiti-cloud-acceptance-scenarios/runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/Tasks.java
+++ activiti-cloud-acceptance-scenarios/runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/Tasks.java
@@ -28,7 +28,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import net.serenitybdd.core.Serenity;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.steps.StepEventBus;
