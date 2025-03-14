92b0dcaec38c833b26bcb4b22776be46add1bc1b
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2025-03-14 18:53:39 +0100

    AAE-31941 fix storage of ephemeral variable
    - Setter is needed for ephemeral attribute in Cloud event
    - Database schema should be updated for audit to include the new field
    - Converters should be updated in audit
    
    WIP: missing migration scripts for db other  than h2

---
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-model/src/test/java/org/activiti/cloud/services/audit/jpa/converters/VariableCreatedEventConverterTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.services.audit.jpa.converters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.activiti.api.model.shared.event.VariableEvent;
+import org.junit.jupiter.api.Test;
+
+class VariableCreatedEventConverterTest {
+
+    private final VariableCreatedEventConverter variableCreatedEventConverter = new VariableCreatedEventConverter(
+        new EventContextInfoAppender()
+    );
+
+    @Test
+    void should_supportVariableCreatedEvent() {
+        assertThat(variableCreatedEventConverter.getSupportedEvent())
+            .isEqualTo(VariableEvent.VariableEvents.VARIABLE_CREATED.name());
+    }
+}
