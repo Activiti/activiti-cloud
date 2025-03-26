715fad9b0cc38985b165b3cc68d7a92b7bdf45c1
revatilimaye <revati.limaye@hyland.com>
2025-03-26 21:36:57 +0530

    AAE-33087 fix test

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/ProcessCreatedEventHandlerTest.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/ProcessCreatedEventHandlerTest.java
@@ -15,7 +15,6 @@
  */
 package org.activiti.cloud.services.query.events.handlers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
