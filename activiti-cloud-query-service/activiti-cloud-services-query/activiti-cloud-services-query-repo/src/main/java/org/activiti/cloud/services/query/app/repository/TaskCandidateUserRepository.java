52976355ba48cafaca43945688611abc6867ef95
jesty <davidecerbo@gmail.com>
2025-05-05 23:20:40 +0200

    AAE-34337 retry on event discard

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-repo/src/main/java/org/activiti/cloud/services/query/app/repository/TaskCandidateUserRepository.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-repo/src/main/java/org/activiti/cloud/services/query/app/repository/TaskCandidateUserRepository.java
@@ -20,6 +20,7 @@ import static org.activiti.cloud.services.query.app.repository.QuerydslBindingsH
 import com.querydsl.core.types.dsl.StringPath;
 import java.util.Collection;
 import java.util.Set;
+import java.util.Set;
 import org.activiti.cloud.services.query.model.QTaskCandidateUserEntity;
 import org.activiti.cloud.services.query.model.TaskCandidateUserEntity;
 import org.activiti.cloud.services.query.model.TaskCandidateUserId;
@@ -43,4 +44,6 @@ public interface TaskCandidateUserRepository
 
         bindings.bind(String.class).first((StringPath path, String value) -> path.eq(value));
     }
+
+    Set<TaskCandidateUserEntity> findByTaskId(String id);
 }
