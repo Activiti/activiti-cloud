6625278655947398ed9ba3149170f2c63fdccd32
Wojciech Piotrowiak <wojciech.piotrowiak@hyland.com>
2025-05-15 17:18:49 +0200

    refactor: replace PostgreSQL with H2 for testing and update Hibernate version

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/ProcessDeployedEventHandlerIT.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/ProcessDeployedEventHandlerIT.java
@@ -23,7 +23,6 @@ import org.activiti.api.runtime.model.impl.ProcessDefinitionImpl;
 import org.activiti.cloud.api.process.model.impl.events.CloudProcessDeployedEventImpl;
 import org.activiti.cloud.services.query.model.ProcessDefinitionEntity;
 import org.activiti.cloud.services.query.model.ProcessModelEntity;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
