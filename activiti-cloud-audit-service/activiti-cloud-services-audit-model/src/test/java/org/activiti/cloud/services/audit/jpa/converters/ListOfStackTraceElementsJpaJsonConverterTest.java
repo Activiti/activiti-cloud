b2168de5907c5147445a7a6f3b25d97b9184acf6
Sourav Bhattacharyya <121745336+Sourav-Bhattacharyya@users.noreply.github.com>
2025-07-07 17:23:43 +0530

    Update ListOfStackTraceElementsJpaJsonConverterTest.java

--- activiti-cloud-audit-service/activiti-cloud-services-audit-model/src/test/java/org/activiti/cloud/services/audit/jpa/converters/ListOfStackTraceElementsJpaJsonConverterTest.java
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-model/src/test/java/org/activiti/cloud/services/audit/jpa/converters/ListOfStackTraceElementsJpaJsonConverterTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 
 public class ListOfStackTraceElementsJpaJsonConverterTest {
 
-    private final ListOfStackTraceElementsJpaJsonConverter converter = new ListOfStackTraceElementsJpaJsonConverter();
+    private ListOfStackTraceElementsJpaJsonConverter converter = new ListOfStackTraceElementsJpaJsonConverter();
 
     @Test
     public void convertToDatabaseColumnShouldReturnTheEntityJsonRepresentation() throws Exception {
