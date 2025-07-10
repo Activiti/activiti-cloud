7aedebed7182312f9831551294e94e0dbaf45399
Sourav Bhattacharyya <121745336+Sourav-Bhattacharyya@users.noreply.github.com>
2025-07-10 16:05:23 +0530

    Prettier Fix

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/ProcessDefinitionControllerImplIT.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/ProcessDefinitionControllerImplIT.java
@@ -288,10 +288,10 @@ class ProcessDefinitionControllerImplIT {
 
         assertThatJson(result.getResponse().getContentAsString()).inPath("entry.id").isEqualTo(procDefId);
         assertThatJson(result.getResponse().getContentAsString()).inPath("entry.name").isEqualTo("my process");
-        assertThatJson(result.getResponse().getContentAsString()).inPath("entry.description").isEqualTo("This is my process");
         assertThatJson(result.getResponse().getContentAsString())
-            .inPath("entry.version")
-            .isEqualTo(1);
+            .inPath("entry.description")
+            .isEqualTo("This is my process");
+        assertThatJson(result.getResponse().getContentAsString()).inPath("entry.version").isEqualTo(1);
     }
 
     @Test
