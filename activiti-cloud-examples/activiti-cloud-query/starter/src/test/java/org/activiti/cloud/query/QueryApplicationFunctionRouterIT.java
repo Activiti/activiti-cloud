fd4b4d33db8026e33890ebc53d0b8a8825a28013
Igor Dianov <igor@dianov.com>
2025-07-17 11:59:25 -0700

    Add QueryApplicationFunctionRouterIT

---
+++ activiti-cloud-examples/activiti-cloud-query/starter/src/test/java/org/activiti/cloud/query/QueryApplicationFunctionRouterIT.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2017-2020 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.activiti.cloud.query;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.activiti.cloud.common.messaging.ActivitiCloudMessagingProperties;
+import org.activiti.cloud.services.test.containers.KeycloakContainerApplicationInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.stream.config.BindingServiceProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(
+    classes = { QueryApplication.class },
+    properties = { "activiti.cloud.messaging.function-router.enabled=true" }
+)
+@Testcontainers
+@ContextConfiguration(initializers = { KeycloakContainerApplicationInitializer.class })
+public class QueryApplicationFunctionRouterIT {
+
+    @ServiceConnection
+    @Container
+    static final RabbitMQContainer rabbitMq = new RabbitMQContainer("rabbitmq:3.8.6-management-alpine");
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @Autowired
+    private BindingServiceProperties bindingServiceProperties;
+
+    @Autowired
+    private ActivitiCloudMessagingProperties messagingProperties;
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void bindingServiceProperties() {
+        assertThat(bindingServiceProperties.getBindings())
+            .doesNotContainKeys("auditConsumer", "queryConsumer")
+            .containsKey("functionRouterInput");
+    }
+
+    @Test
+    void functionRouter() {
+        var functionRouter = messagingProperties.getFunctionRouter();
+
+        assertThat(functionRouter.isEnabled()).isTrue();
+
+        assertThat(functionRouter.getFunctionRoutes()).containsOnly("auditConsumer", "queryConsumer");
+    }
+
+    @Test
+    void environment() {
+        assertThat(
+            environment.getProperty(
+                "spring.cloud.stream.rabbit.bindings.functionRouterInput.consumer.queue-name-group-only",
+                Boolean.class
+            )
+        )
+            .isTrue();
+
+        assertThat(environment.getProperty("activiti.cloud.messaging.function-router.group", String.class))
+            .isEqualTo("query");
+    }
+}
