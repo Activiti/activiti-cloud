73ea4396b6f4125fc6ff26c08937448e880fcf0b
Ayan Dasgupta <ayan.dasgupta@hyland.com>
2025-08-01 05:40:18 -0400

    ran pre-commit and checked spelling issues

--- activiti-cloud-audit-service/activiti-cloud-services-audit-rest/src/test/java/org/activiti/cloud/services/audit/jpa/controller/DeleteMeExternalIT.java
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-rest/src/test/java/org/activiti/cloud/services/audit/jpa/controller/DeleteMeExternalIT.java
@@ -26,7 +26,7 @@ public class DeleteMeExternalIT {
 
     @Test
     void coveredITExternally() {
-        assertThat(deleteMe.covertedITLocally())
+        assertThat(deleteMe.coveredITLocally())
             .isEqualTo("this is a test method that is covered by integration tests in the same module");
     }
 }
