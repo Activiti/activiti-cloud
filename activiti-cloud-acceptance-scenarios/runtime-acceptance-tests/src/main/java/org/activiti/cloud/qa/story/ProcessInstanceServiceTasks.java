6e676fe4975401a4ea87f6bd159423469e416d5f
Wojciech Piotrowiak <wojciech.piotrowiak@hyland.com>
2025-08-11 12:59:00 +0200

    fix: refactor reporting of element events for improved readability

--- activiti-cloud-acceptance-scenarios/runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/ProcessInstanceServiceTasks.java
+++ activiti-cloud-acceptance-scenarios/runtime-acceptance-tests/src/main/java/org/activiti/cloud/qa/story/ProcessInstanceServiceTasks.java
@@ -436,9 +436,12 @@ public class ProcessInstanceServiceTasks {
             );
         });
 
-        Serenity.reportThat("Element Events Detail", () -> {
-            assertThat(elementEvents).isNotEmpty();
-        });
+        Serenity.reportThat(
+            "Element Events Detail",
+            () -> {
+                assertThat(elementEvents).isNotEmpty();
+            }
+        );
         Serenity.recordReportData().withTitle("Element Events Detail").andContents(eventDetails.toString());
     }
 
