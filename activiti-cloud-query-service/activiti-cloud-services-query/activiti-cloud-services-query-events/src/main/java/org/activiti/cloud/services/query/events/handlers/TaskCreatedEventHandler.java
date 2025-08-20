893221d8a1bc79dc8f0aa9c6462bd646e2dd4bbd
Michael Coune <michael.coune@hyland.com>
2025-08-20 08:19:24 +0200

    prettier

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/main/java/org/activiti/cloud/services/query/events/handlers/TaskCreatedEventHandler.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/main/java/org/activiti/cloud/services/query/events/handlers/TaskCreatedEventHandler.java
@@ -60,7 +60,8 @@ public class TaskCreatedEventHandler implements QueryEventHandler {
                                         processInstanceEntity.getProcessDefinitionName()
                                     );
                                     queryTaskEntity.setProcessVariables(processInstanceEntity.getVariables());
-                                    queryTaskEntity.setTaskProcessRootProcessInstanceId(processInstanceEntity.getRootProcessInstanceId()
+                                    queryTaskEntity.setTaskProcessRootProcessInstanceId(
+                                        processInstanceEntity.getRootProcessInstanceId()
                                     );
 
                                     processInstanceEntity.getTasks().add(queryTaskEntity);
--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/TaskCreatedEventHandlerTest.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/TaskCreatedEventHandlerTest.java
@@ -77,7 +77,8 @@ public class TaskCreatedEventHandlerTest {
         assertThat(taskEntity.getId()).isEqualTo(expectedEventEntity.getId());
         assertThat(taskEntity.getName()).isEqualTo(expectedEventEntity.getName());
         assertThat(taskEntity.getProcessInstanceId()).isEqualTo(expectedEventEntity.getProcessInstanceId());
-        assertThat(taskEntity.getTaskProcessRootProcessInstanceId()).isEqualTo(processInstanceEntity.getRootProcessInstanceId());
+        assertThat(taskEntity.getTaskProcessRootProcessInstanceId())
+            .isEqualTo(processInstanceEntity.getRootProcessInstanceId());
     }
 
     private ProcessInstanceEntity buildProcessInstanceEntity() {
