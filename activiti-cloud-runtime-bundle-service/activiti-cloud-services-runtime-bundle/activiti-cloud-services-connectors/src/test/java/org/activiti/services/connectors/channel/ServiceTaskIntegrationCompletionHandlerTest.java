413bbd7b06451454f80e983522b597970dd2678c
Wojciech Piotrowiak <wojciech.piotrowiak@hyland.com>
2025-08-29 20:53:20 +0200

    AAE-35904 Error handling improvement

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-connectors/src/test/java/org/activiti/services/connectors/channel/ServiceTaskIntegrationCompletionHandlerTest.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-connectors/src/test/java/org/activiti/services/connectors/channel/ServiceTaskIntegrationCompletionHandlerTest.java
@@ -16,6 +16,7 @@
 
 package org.activiti.services.connectors.channel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
