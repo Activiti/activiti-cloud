153554a9f9051f9dcad7203621314b625e52613c
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2025-09-02 15:47:47 +0200

    AAE-36461 Re-add async

--- activiti-cloud-messages-service/services/core/src/main/java/org/activiti/cloud/services/messages/core/integration/MessageConnectorIntegrationFlow.java
+++ activiti-cloud-messages-service/services/core/src/main/java/org/activiti/cloud/services/messages/core/integration/MessageConnectorIntegrationFlow.java
@@ -99,6 +99,7 @@ public class MessageConnectorIntegrationFlow extends IntegrationFlowAdapter {
                         .transactional()
                         .id(MESSAGE_GATEWAY)
                         .requiresReply(false)
+                        .async(true)
                         .replyTimeout(0L)
                         .advice(interceptor)
             );
