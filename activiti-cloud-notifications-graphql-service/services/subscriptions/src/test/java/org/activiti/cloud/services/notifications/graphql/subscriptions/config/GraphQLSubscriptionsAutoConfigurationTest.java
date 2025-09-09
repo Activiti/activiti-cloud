0630e10ccb028599eef70065a62de48e41bd9c64
Federico D'Ulizia <federico.dulizia@hyland.com>
2025-09-09 11:30:16 +0200

    AAE-36828 pre-commit

--- activiti-cloud-notifications-graphql-service/services/subscriptions/src/test/java/org/activiti/cloud/services/notifications/graphql/subscriptions/config/GraphQLSubscriptionsAutoConfigurationTest.java
+++ activiti-cloud-notifications-graphql-service/services/subscriptions/src/test/java/org/activiti/cloud/services/notifications/graphql/subscriptions/config/GraphQLSubscriptionsAutoConfigurationTest.java
@@ -61,7 +61,7 @@ public class GraphQLSubscriptionsAutoConfigurationTest {
                     "processDefinitionKey",
                     "processInstanceId",
                     "businessKey",
-                    "initiator"
+                    "initiator",
                 }
             );
     }
