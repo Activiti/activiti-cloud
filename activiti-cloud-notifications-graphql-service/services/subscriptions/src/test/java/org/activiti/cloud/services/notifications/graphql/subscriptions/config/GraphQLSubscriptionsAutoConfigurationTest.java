171068d4ef10ea0fd644d1b5a602fa57dc229111
Federico D'Ulizia <federico.dulizia@hyland.com>
2025-09-15 17:25:12 +0200

    AAE-36828 replace 'actor' with 'initiator' in GraphQL subscription queries and tests

--- activiti-cloud-notifications-graphql-service/services/subscriptions/src/test/java/org/activiti/cloud/services/notifications/graphql/subscriptions/config/GraphQLSubscriptionsAutoConfigurationTest.java
+++ activiti-cloud-notifications-graphql-service/services/subscriptions/src/test/java/org/activiti/cloud/services/notifications/graphql/subscriptions/config/GraphQLSubscriptionsAutoConfigurationTest.java
@@ -61,7 +61,7 @@ public class GraphQLSubscriptionsAutoConfigurationTest {
                     "processDefinitionKey",
                     "processInstanceId",
                     "businessKey",
-                    "actor",
+                    "initiator",
                 }
             );
     }
