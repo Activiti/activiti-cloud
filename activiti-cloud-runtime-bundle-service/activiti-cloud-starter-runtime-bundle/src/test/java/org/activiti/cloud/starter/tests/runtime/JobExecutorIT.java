470d31969961546faa75dd140e0643bc7646dca7
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2025-09-15 17:52:06 +0200

    AAE-36461 Revert

--- activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/JobExecutorIT.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/runtime/JobExecutorIT.java
@@ -194,8 +194,6 @@ public class JobExecutorIT {
     @AfterEach
     public void tearDown() {
         processEngineConfiguration.getClock().reset();
-        RetryFailingDelegate.shallThrow = false;
-        RetryFailingDelegate.resetTimeList();
     }
 
     @Test
