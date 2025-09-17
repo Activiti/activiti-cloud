fbbcce7a77c6f77fa9a9a185e03656c71aa1a1e5
Aleksandra Onych <aleksandra.onych@hyland.com>
2025-09-17 15:04:58 +0200

    [AAE-36461] add missing methods

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/springframework/cloud/function/context/catalog/FunctionTypeUtils.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/springframework/cloud/function/context/catalog/FunctionTypeUtils.java
@@ -15,7 +15,10 @@
  */
 package org.springframework.cloud.function.context.catalog;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.cloud.function.context.catalog.SimpleFunctionRegistry.FunctionInvocationWrapper;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
@@ -41,6 +44,20 @@ public final class FunctionTypeUtils {
      */
     public static Type functionType(Type input, Type output) {
         return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.functionType(input, output);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type consumerType(Type input) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.consumerType(input);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type supplierType(Type output) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.supplierType(output);
     }
 
     /**
@@ -87,6 +104,14 @@ public final class FunctionTypeUtils {
 
     /**
      * Delegates to our custom FunctionTypeUtils implementation.
+     * This is the missing method that was causing the NoSuchMethodError.
+     */
+    public static boolean isCollectionOfMessage(Type type) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isCollectionOfMessage(type);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
      */
     public static boolean isTypeCollection(Type type) {
         return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isTypeCollection(type);
@@ -95,22 +120,15 @@ public final class FunctionTypeUtils {
     /**
      * Delegates to our custom FunctionTypeUtils implementation.
      */
+    public static boolean isTypeMap(Type type) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isTypeMap(type);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
     public static boolean isTypeArray(Type type) {
         return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isTypeArray(type);
-    }
-
-    /**
-     * Delegates to our custom FunctionTypeUtils implementation.
-     */
-    public static Type getGenericType(Type type) {
-        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getGenericType(type);
-    }
-
-    /**
-     * Delegates to our custom FunctionTypeUtils implementation.
-     */
-    public static Type getImmediateGenericType(Type type, int index) {
-        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getImmediateGenericType(type, index);
     }
 
     /**
@@ -141,10 +159,115 @@ public final class FunctionTypeUtils {
         return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isMono(type);
     }
 
-    // Add any other methods that might be called by Spring Cloud Function
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type getGenericType(Type type) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getGenericType(type);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type getImmediateGenericType(Type type, int index) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getImmediateGenericType(type, index);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Method discoverFunctionalMethod(Class<?> pojoFunctionClass) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverFunctionalMethod(pojoFunctionClass);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type discoverFunctionTypeFromClass(Class<?> functionalClass) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverFunctionTypeFromClass(functionalClass);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type discoverFunctionTypeFromFunctionFactoryMethod(Class<?> clazz, String methodName) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverFunctionTypeFromFunctionFactoryMethod(clazz, methodName);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type discoverFunctionTypeFromFunctionFactoryMethod(Method method) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverFunctionTypeFromFunctionFactoryMethod(method);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type discoverFunctionTypeFromFunctionMethod(Method functionMethod) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverFunctionTypeFromFunctionMethod(functionMethod);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static int getInputCount(FunctionInvocationWrapper function) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getInputCount(function);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static int getOutputCount(FunctionInvocationWrapper function) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getOutputCount(function);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type getComponentTypeOfInputType(Type functionType) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getComponentTypeOfInputType(functionType);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static Type getComponentTypeOfOutputType(Type functionType) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.getComponentTypeOfOutputType(functionType);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static String discoverBeanDefinitionNameByQualifier(ListableBeanFactory beanFactory, String qualifier) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.discoverBeanDefinitionNameByQualifier(beanFactory, qualifier);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static boolean isOutputArray(Type functionType) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isOutputArray(functionType);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static boolean isMultipleArgumentType(Type type) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isMultipleArgumentType(type);
+    }
+
+    /**
+     * Delegates to our custom FunctionTypeUtils implementation.
+     */
+    public static boolean isJsonNode(Type type) {
+        return org.activiti.cloud.common.messaging.util.FunctionTypeUtils.isJsonNode(type);
+    }
+
+    // Add any other methods that might be called by Spring Cloud Function internal code
     // All methods delegate to our custom implementation
 
     static {
-        System.out.println("SUCCESS: Shadow FunctionTypeUtils loaded - all calls will be delegated to Activiti Cloud's custom implementation");
+        System.out.println("SUCCESS: Complete Shadow FunctionTypeUtils loaded - all calls will be delegated to Activiti Cloud's custom implementation");
     }
 }
