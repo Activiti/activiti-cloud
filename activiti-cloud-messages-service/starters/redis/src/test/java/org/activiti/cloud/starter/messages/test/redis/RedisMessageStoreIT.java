28a56076ae0238fd42267cd59785019f271d436a
Aleksandra Onych <aleksandra.onych@hyland.com>
2025-09-19 16:02:49 +0200

    Revert "[AAE-36461] Comment out problematic test"
    This reverts commit 8a57cd1239d9a2a8282598b188522dd33eaa7035.

--- activiti-cloud-messages-service/starters/redis/src/test/java/org/activiti/cloud/starter/messages/test/redis/RedisMessageStoreIT.java
+++ activiti-cloud-messages-service/starters/redis/src/test/java/org/activiti/cloud/starter/messages/test/redis/RedisMessageStoreIT.java
@@ -13,39 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//package org.activiti.cloud.starter.messages.test.redis;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import org.activiti.cloud.services.messages.tests.AbstractMessagesCoreIntegrationTests;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.parallel.Isolated;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.integration.redis.store.RedisMessageStore;
-//import org.springframework.integration.transaction.PseudoTransactionManager;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//@Isolated
-//@ContextConfiguration(initializers = RedisApplicationInitializer.class)
-//public class RedisMessageStoreIT extends AbstractMessagesCoreIntegrationTests {
-//
-//    @SpringBootApplication
-//    static class MessagesApplication {}
-//
-//    @TestConfiguration
-//    static class Configuration {
-//
-//        @Bean
-//        public PlatformTransactionManager transactionManager() {
-//            return new PseudoTransactionManager();
-//        }
-//    }
-//
-//    @Test
-//    public void testMessageStore() {
-//        assertThat(this.aggregatingMessageHandler.getMessageStore()).isInstanceOf(RedisMessageStore.class);
-//    }
-//}
+package org.activiti.cloud.starter.messages.test.redis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.activiti.cloud.services.messages.tests.AbstractMessagesCoreIntegrationTests;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.redis.store.RedisMessageStore;
+import org.springframework.integration.transaction.PseudoTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Isolated
+@ContextConfiguration(initializers = RedisApplicationInitializer.class)
+public class RedisMessageStoreIT extends AbstractMessagesCoreIntegrationTests {
+
+    @SpringBootApplication
+    static class MessagesApplication {}
+
+    @TestConfiguration
+    static class Configuration {
+
+        @Bean
+        public PlatformTransactionManager transactionManager() {
+            return new PseudoTransactionManager();
+        }
+    }
+
+    @Test
+    public void testMessageStore() {
+        assertThat(this.aggregatingMessageHandler.getMessageStore()).isInstanceOf(RedisMessageStore.class);
+    }
+}
