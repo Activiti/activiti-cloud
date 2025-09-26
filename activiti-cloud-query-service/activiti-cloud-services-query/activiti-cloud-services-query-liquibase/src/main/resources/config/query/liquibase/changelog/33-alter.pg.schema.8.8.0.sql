cb197066f9989d92c71c31d21c5e14744f5dc292
George Walter Colgrove IV <georgecolgrove@outlook.com>
2025-09-25 20:56:05 -0400

    add index

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/33-alter.pg.schema.8.8.0.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/33-alter.pg.schema.8.8.0.sql
@@ -4,3 +4,5 @@ ALTER TABLE task_variable
       (
       jsonb_to_tsvector('simple', value, '["string", "numeric"]')
       ) STORED
+
+CREATE INDEX table_col_fts_idx ON task_variable USING gin (fts)
