203861e6ef391a199cdb1ea5142511e2b7f77bd4
Igor Dianov <igdianov@users.noreply.github.com>
2025-10-17 12:51:04 -0700

    AAE-35743 Fix multiplex message bindings configuration with empty connectorType (#1974)

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/test/java/org/activiti/cloud/common/messaging/config/test/FunctionRouterBindingConfigurationIT.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/test/java/org/activiti/cloud/common/messaging/config/test/FunctionRouterBindingConfigurationIT.java
@@ -198,7 +198,7 @@ public class FunctionRouterBindingConfigurationIT {
         }
 
         @Bean
-        @ConnectorBinding(input = SCRIPT_RUNTIME_CONSUMER, connectorType = "script.EXECUTE", condition = "true")
+        @ConnectorBinding(input = SCRIPT_RUNTIME_CONSUMER, condition = "true")
         public ConsumerConnector<String> scriptRuntimeExecutor() {
             return message -> {
                 connectorPayload.set(message);
@@ -218,7 +218,7 @@ public class FunctionRouterBindingConfigurationIT {
         }
 
         @Bean
-        @ConnectorBinding(input = REST_CONSUMER, connectorType = "rest.GET", condition = "true")
+        @ConnectorBinding(input = REST_CONSUMER, connectorType = "${FOOBAR:rest.GET}", condition = "true")
         public ConsumerConnector<String> restConsumerGetHandler() {
             return message -> {
                 getPayload.set(message);
