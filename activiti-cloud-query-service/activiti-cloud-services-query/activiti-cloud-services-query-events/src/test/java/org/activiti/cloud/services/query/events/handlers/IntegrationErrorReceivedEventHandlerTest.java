6a0bd1952758874a2442a3b1b908604aef174892
Aleksandra Onych <aleksandra.onych@hyland.com>
2025-12-02 12:06:02 +0100

    Update activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/IntegrationErrorReceivedEventHandlerTest.java
    Co-authored-by: Domenico Sibilio <domenicosibilio@gmail.com>

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/IntegrationErrorReceivedEventHandlerTest.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-events/src/test/java/org/activiti/cloud/services/query/events/handlers/IntegrationErrorReceivedEventHandlerTest.java
@@ -38,7 +38,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class IntegrationErrorReceivedEventHandlerTest extends IntegrationEventsHelper {
+class IntegrationErrorReceivedEventHandlerTest extends IntegrationEventsHelper {
 
     @InjectMocks
     private IntegrationErrorReceivedEventHandler errorHandler;
