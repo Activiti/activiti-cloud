5a0dea2c58c6a23878c0cfd2b9146907d0191151
Giovanni Fertuso <44156864+Giovanni007@users.noreply.github.com>
2025-12-05 18:23:59 +0100

    AAE-40256 Remove process definition category filter test (#2057)
    * chore(deps): upgrade Activiti/Activiti to version 8.8.0-alpha.126
    
    from: https://github.com/Activiti/Activiti
    
    * AAE-40256 Remove process definition category filter test
    
    * fix
    
    * Revert "chore(deps): upgrade Activiti/Activiti to version 8.8.0-alpha.126"
    
    This reverts commit 5a163eba1021d2c629539b90d8dfec113df796d5.
    
    ---------
    
    Co-authored-by: alfresco-build <>

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/ProcessDefinitionAdminControllerImplIT.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/test/java/org/activiti/cloud/services/rest/controllers/ProcessDefinitionAdminControllerImplIT.java
@@ -174,8 +174,6 @@ class ProcessDefinitionAdminControllerImplIT {
 
         this.mockMvc.perform(get("/admin/v1/process-definitions").accept(MediaTypes.HAL_JSON_VALUE))
             .andExpect(status().isOk());
-
-        assertThat(payloadCaptor.getValue().getProcessCategoryToExclude()).isNull();
     }
 
     @Test
