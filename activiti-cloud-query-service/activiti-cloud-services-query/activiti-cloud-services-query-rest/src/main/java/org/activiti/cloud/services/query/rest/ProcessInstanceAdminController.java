1a7d2254613c80980167faf4b1fcc779696efe9f
Federico D'Ulizia <federico.dulizia@hyland.com>
2026-01-09 10:55:30 +0100

    AAE-39671 Add endpoint to retrieve linked processes by instance ID (#2085)
    * AAE-39671 Add endpoint to retrieve linked processes by instance ID

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/ProcessInstanceAdminController.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/ProcessInstanceAdminController.java
@@ -44,6 +44,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -203,6 +204,19 @@ public class ProcessInstanceAdminController {
         return pagedCollectionModelAssembler.toModel(
             pageable,
             processInstanceAdminControllerHelper.searchSubprocesses(processInstanceId, predicate, pageable),
+            processInstanceRepresentationModelAssembler
+        );
+    }
+
+    @JsonView(JsonViews.General.class)
+    @GetMapping(value = "/{linkedProcessInstanceId}/linkedprocesses")
+    public PagedModel<EntityModel<QueryCloudProcessInstance>> linkedProcesses(
+        @PathVariable String linkedProcessInstanceId,
+        Pageable pageable
+    ) {
+        return pagedCollectionModelAssembler.toModel(
+            pageable,
+            processInstanceAdminControllerHelper.searchLinkedProcesses(linkedProcessInstanceId, pageable),
             processInstanceRepresentationModelAssembler
         );
     }
