7ffc6b08ba6cfc3d415d2e51a337634f8b5592a9
Aleksandra Bielecka <aleksandra.bielecka@hyland.com>
2026-01-13 12:46:07 +0100

    [AAE-39413] Add on delete cascade to foreign key

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/34-alter.pg.schema.8.8.0.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/34-alter.pg.schema.8.8.0.sql
@@ -4,4 +4,5 @@ ALTER TABLE integration_context
 ALTER TABLE integration_context
   ADD CONSTRAINT fk_integration_context_bpmn_activity
   FOREIGN KEY (process_instance_id, client_id, execution_id)
-  REFERENCES bpmn_activity (process_instance_id, element_id, execution_id);
+  REFERENCES bpmn_activity (process_instance_id, element_id, execution_id)
+  ON DELETE CASCADE;
--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/h2.schema.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/h2.schema.sql
@@ -305,7 +305,8 @@ create index pcsu_processDefinition_idx on process_candidate_starter_user (proce
 alter table integration_context
   add constraint fk_integration_context_bpmn_activity
   foreign key (process_instance_id, client_id, execution_id)
-  references bpmn_activity (process_instance_id, element_id, execution_id);
+  references bpmn_activity (process_instance_id, element_id, execution_id)
+  on delete cascade;
 alter table process_model
     add constraint FKmqdabtfsoy52f0585vkfj40b foreign key (process_definition_id) references process_definition;
 alter table task_process_variable
