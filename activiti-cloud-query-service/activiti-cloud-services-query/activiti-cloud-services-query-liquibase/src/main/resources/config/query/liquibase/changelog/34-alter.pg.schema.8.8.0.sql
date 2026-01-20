4cf14962a2f85c37f3dc94e0b2eb14c835731c12
Aleksandra Bielecka <aleksandra.bielecka@hyland.com>
2026-01-20 16:20:58 +0100

    [AAE-39413] Revert - purge orphaned integration_context records

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/34-alter.pg.schema.8.8.0.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/34-alter.pg.schema.8.8.0.sql
@@ -1,15 +1,6 @@
 ALTER TABLE integration_context
   DROP CONSTRAINT integration_context_bpmn_activity_idx;
 
--- Purge orphaned integration_context records that don't have a matching bpmn_activity entity
-DELETE FROM integration_context ic
-WHERE NOT EXISTS (
-  SELECT 1 FROM bpmn_activity ba
-  WHERE ba.process_instance_id = ic.process_instance_id
-    AND ba.element_id = ic.client_id
-    AND ba.execution_id = ic.execution_id
-);
-
 ALTER TABLE integration_context
   ADD CONSTRAINT fk_integration_context_bpmn_activity
   FOREIGN KEY (process_instance_id, client_id, execution_id)
