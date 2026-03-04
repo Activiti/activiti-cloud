bf854bbd9835871343aff8b2ed87e6aa7a1d23ce
Tommaso D'Alessandro <tommaso.dalessandro@hyland.com>
2026-03-04 09:51:01 +0100

    AAE-42735 Fix scripts

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/35-alter.pg.schema.8.8.0.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/35-alter.pg.schema.8.8.0.sql
@@ -1,7 +1,7 @@
 ALTER TABLE process_instance
   ADD COLUMN IF NOT EXISTS type VARCHAR(255) NOT NULL DEFAULT (
     CASE
-      WHEN parent_id IS NOT NULL OR parent_id != id THEN 'call-activity'
+      WHEN parent_id IS NOT NULL AND parent_id != id THEN 'call-activity'
       WHEN linked_process_instance_type IS NOT NULL THEN linked_process_instance_type
       ELSE 'main-process'
       END
