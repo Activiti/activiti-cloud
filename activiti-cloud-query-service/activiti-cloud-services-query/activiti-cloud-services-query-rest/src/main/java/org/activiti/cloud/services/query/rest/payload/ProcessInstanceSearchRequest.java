d14b9d3c2a35d0d3ef58cd8270386fdfba026af3
Federico D'Ulizia <federico.dulizia@hyland.com>
2026-03-06 11:22:07 +0100

    pre-commit

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/payload/ProcessInstanceSearchRequest.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/payload/ProcessInstanceSearchRequest.java
@@ -15,9 +15,9 @@
  */
 package org.activiti.cloud.services.query.rest.payload;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Date;
 import java.util.Set;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.cloud.services.query.model.ProcessVariableKey;
 import org.activiti.cloud.services.query.rest.filter.VariableFilter;
@@ -48,6 +48,7 @@ public class ProcessInstanceSearchRequest implements CloudRuntimeEntityFilterReq
     private Set<String> processRelatedTo;
     private Boolean includeUnlinkedProcesses;
     private Boolean includeLinkedProcesses;
+
     @JsonIgnore
     private Set<String> subprocessParentIds;
 
