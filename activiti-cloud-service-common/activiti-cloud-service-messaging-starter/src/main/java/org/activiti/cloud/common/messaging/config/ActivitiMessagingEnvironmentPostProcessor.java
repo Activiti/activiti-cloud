2f2958d8233020f08eb287936e0e1b33cbcae9cb
Edoardo Patti <Edoardo.Patti@hyland.com>
2026-03-18 15:28:18 +0100

    AAE-43226 EnvironmentPostProcessor update

--- activiti-cloud-service-common/activiti-cloud-service-messaging-starter/src/main/java/org/activiti/cloud/common/messaging/config/ActivitiMessagingEnvironmentPostProcessor.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-starter/src/main/java/org/activiti/cloud/common/messaging/config/ActivitiMessagingEnvironmentPostProcessor.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.activiti.cloud.common.messaging.ActivitiCloudMessagingProperties.MessagingBroker;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 
