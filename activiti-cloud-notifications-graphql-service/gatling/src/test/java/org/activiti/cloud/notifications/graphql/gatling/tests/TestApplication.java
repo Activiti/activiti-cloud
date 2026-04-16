70f3fd1126975627c277009bdc4f0c9a4d437fc2
Edoardo Patti <Edoardo.Patti@hyland.com>
2026-04-16 18:03:08 +0200

    test fix

--- activiti-cloud-notifications-graphql-service/gatling/src/test/java/org/activiti/cloud/notifications/graphql/gatling/tests/TestApplication.java
+++ activiti-cloud-notifications-graphql-service/gatling/src/test/java/org/activiti/cloud/notifications/graphql/gatling/tests/TestApplication.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -28,7 +29,8 @@ public class TestApplication {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
+            .waitingFor(Wait.forListeningPort());
     }
 
     public static void main(String[] args) {
