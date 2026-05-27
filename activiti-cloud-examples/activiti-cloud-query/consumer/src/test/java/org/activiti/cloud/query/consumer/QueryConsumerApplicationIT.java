f195b8572548f426ba885d338650d8fd6244b9f2
Stephen Donnell <stephen.donnell@hyland.com>
2026-05-27 13:26:52 -0500

    AAE-41740 Fixing prettier configuration, maybe?

--- activiti-cloud-examples/activiti-cloud-query/consumer/src/test/java/org/activiti/cloud/query/consumer/QueryConsumerApplicationIT.java
+++ activiti-cloud-examples/activiti-cloud-query/consumer/src/test/java/org/activiti/cloud/query/consumer/QueryConsumerApplicationIT.java
@@ -104,6 +104,8 @@ public class QueryConsumerApplicationIT {
 
     @Test
     void rabbitExchanges() {
-        assertThat(binderFactoryListenerTestContext.getExchanges()).isNotEmpty().containsOnlyKeys("engineEvents", "projectedEngineEventsProducer");
+        assertThat(binderFactoryListenerTestContext.getExchanges())
+            .isNotEmpty()
+            .containsOnlyKeys("engineEvents", "projectedEngineEventsProducer");
     }
 }
