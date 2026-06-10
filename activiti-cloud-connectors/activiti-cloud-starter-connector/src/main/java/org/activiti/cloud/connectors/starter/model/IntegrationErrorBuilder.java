a0ab391bb040925ddcc3b9e8f70eec8d94089842
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2026-06-10 09:23:07 +0200

    AAE-46511 Improve guardrail audit error message

--- activiti-cloud-connectors/activiti-cloud-starter-connector/src/main/java/org/activiti/cloud/connectors/starter/model/IntegrationErrorBuilder.java
+++ activiti-cloud-connectors/activiti-cloud-starter-connector/src/main/java/org/activiti/cloud/connectors/starter/model/IntegrationErrorBuilder.java
@@ -54,7 +54,9 @@ public class IntegrationErrorBuilder {
         Objects.requireNonNull(error);
 
         IntegrationErrorImpl integrationError;
-        if (error instanceof CloudBpmnError cloudBpmnError && cloudBpmnError.getMessage() != null) {
+        if (error instanceof CloudBpmnError cloudBpmnError &&
+            "BLOCKED_BY_GUARDRAIL".equals(cloudBpmnError.getErrorCode()) &&
+            cloudBpmnError.getMessage() != null) {
             integrationError = new IntegrationErrorImpl(integrationRequest, error, cloudBpmnError.getMessage());
         } else {
             integrationError = new IntegrationErrorImpl(integrationRequest, error);
