795c8cb7598124f2afd5bb4e89786af9d0b58de3
Edoardo Patti <Edoardo.Patti@hyland.com>
2026-06-12 16:48:15 +0200

    AAE-45374 Replace local ObjectMapper/JsonMapper instances with injected or shared ones (#2306)

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/util/ProcessInstanceSearchRequestBuilder.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/util/ProcessInstanceSearchRequestBuilder.java
@@ -16,7 +16,6 @@
 package org.activiti.cloud.services.query.util;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import java.util.Date;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,10 +25,17 @@ import org.activiti.cloud.services.query.rest.filter.VariableFilter;
 import org.activiti.cloud.services.query.rest.payload.CloudRuntimeEntitySort;
 import org.activiti.cloud.services.query.rest.payload.ProcessInstanceSearchRequest;
 import org.springframework.data.domain.Sort;
-import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.introspect.VisibilityChecker;
+import tools.jackson.databind.json.JsonMapper;
 
 public class ProcessInstanceSearchRequestBuilder {
+
+    private static final JsonMapper JSON_MAPPER = JsonMapper
+        .builder()
+        .changeDefaultVisibility((VisibilityChecker checker) ->
+            checker.withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+        )
+        .build();
 
     private Set<String> ids;
     private Set<String> parentIds;
@@ -230,13 +236,7 @@ public class ProcessInstanceSearchRequestBuilder {
     }
 
     public String buildJson() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-            return objectMapper.writeValueAsString(build());
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return JSON_MAPPER.writeValueAsString(build());
     }
 
     public ProcessInstanceSearchRequestBuilder withIncludeLinkedProcesses(Boolean includeLinkedProcesses) {
