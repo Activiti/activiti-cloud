2d7c8b05f5e803bbef6d60dec069886e2ab4f1f7
Stephen Donnell <stephen.donnell@hyland.com>
2026-06-24 10:23:20 -0500

    AAE-41740 Strip kafka_* headers from query events outbox messages

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-consumer/src/main/java/org/activiti/cloud/conf/QueryConsumerAutoConfiguration.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-consumer/src/main/java/org/activiti/cloud/conf/QueryConsumerAutoConfiguration.java
@@ -201,7 +201,7 @@ public class QueryConsumerAutoConfiguration {
     IntegrationFlow queryEventsQueueIntegrationFlow(
         QueueChannel queryEventsChannel,
         @Value(
-            "${activiti.cloud.query.consumer.events.queue.headers-to-remove:sourceData,errorChannel,replyChannel,amqp_*,spring.cloud.function.definition}"
+            "${activiti.cloud.query.consumer.events.queue.headers-to-remove:sourceData,errorChannel,replyChannel,amqp_*,kafka_*,spring.cloud.function.definition}"
         ) String[] headersToRemove
     ) {
         return IntegrationFlow.from("queryEventsQueueIntegrationFlowInput")
