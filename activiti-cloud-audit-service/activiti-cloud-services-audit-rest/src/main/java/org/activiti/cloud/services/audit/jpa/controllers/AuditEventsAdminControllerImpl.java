d96c40f9a09277596d2505950c16180d4513ab0a
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2026-07-02 16:31:24 +0200

    AAE-44864 Remove import

--- activiti-cloud-audit-service/activiti-cloud-services-audit-rest/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsAdminControllerImpl.java
+++ activiti-cloud-audit-service/activiti-cloud-services-audit-rest/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsAdminControllerImpl.java
@@ -48,7 +48,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping(
