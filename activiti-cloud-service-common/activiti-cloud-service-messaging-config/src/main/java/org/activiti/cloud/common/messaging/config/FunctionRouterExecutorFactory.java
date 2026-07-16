c5dab254b27a405a965d7ba1acd6fac7d4682ff1
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2026-07-16 10:47:30 +0200

    AAE-47841 Update error message

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionRouterExecutorFactory.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionRouterExecutorFactory.java
@@ -51,7 +51,7 @@ public class FunctionRouterExecutorFactory implements Function<String, ExecutorS
             // until the queue can accept the task.
             if (!executor.getQueue().offer(runnable, timeout.toMillis(), TimeUnit.MILLISECONDS)) {
                 throw new RejectedExecutionException(
-                    "Timeout after %s duration because the queue is full".formatted(timeout)
+                    "fix/AAE-47841: Timeout after %s duration because the queue is full (queueCapacity=%d)".formatted(timeout, QUEUE_CAPACITY)
                 );
             }
         } catch (InterruptedException e) {
