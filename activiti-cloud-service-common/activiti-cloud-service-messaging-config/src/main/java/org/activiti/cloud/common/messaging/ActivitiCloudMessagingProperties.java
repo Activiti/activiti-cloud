e46ba54563553a73fa78a5eae3f4212cfe4befcb
mikolaj.brzezinski <mikolaj.brzezinski@hyland.com>
2026-07-17 08:06:06 +0200

    AAE-47841 180s timeout

--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/ActivitiCloudMessagingProperties.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/ActivitiCloudMessagingProperties.java
@@ -442,7 +442,7 @@ public class ActivitiCloudMessagingProperties {
 
         private Duration retryInterval = Duration.ofMillis(10);
 
-        private Duration requestTimeout = Duration.ofSeconds(60);
+        private Duration requestTimeout = Duration.ofSeconds(180);
 
         @NestedConfigurationProperty
         private final FunctionRouterAnonymousProperties anonymous = new FunctionRouterAnonymousProperties();
--- activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionRouterExecutorFactory.java
+++ activiti-cloud-service-common/activiti-cloud-service-messaging-config/src/main/java/org/activiti/cloud/common/messaging/config/FunctionRouterExecutorFactory.java
@@ -31,7 +31,7 @@ import java.util.function.Function;
 public class FunctionRouterExecutorFactory implements Function<String, ExecutorService> {
 
     private final Map<String, ExecutorService> executors = new ConcurrentHashMap<>();
-    private Duration timeout = Duration.ofSeconds(60);
+    private Duration timeout = Duration.ofSeconds(180);
     private static final int SINGLE_THREAD_POOL_SIZE = 1;
     private static final int QUEUE_CAPACITY = 5;
 
