1d32839530c8c1dd8dd4f0037ef0e551612e5786
Radoslaw Smogura <radoslaw.smogura@hyland.com>
2026-07-20 19:53:27 +0200

    Fix changes

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/43-alter.pg.schema.9.1.0.sql
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-liquibase/src/main/resources/config/query/liquibase/changelog/43-alter.pg.schema.9.1.0.sql
@@ -2,9 +2,3 @@ DROP INDEX IF EXISTS idx_pvh_record_create_time;
 DROP INDEX IF EXISTS idx_pvh_process_var;
 DROP TABLE IF EXISTS process_variable_history;
 DROP SEQUENCE IF EXISTS process_variable_history_sequence;
-
-ALTER TABLE integration_context
-  ADD COLUMN IF NOT EXISTS externalized_data_provider_type varchar(255);
-
-ALTER TABLE integration_context
-  ADD COLUMN IF NOT EXISTS externalized_data_url varchar(255);
