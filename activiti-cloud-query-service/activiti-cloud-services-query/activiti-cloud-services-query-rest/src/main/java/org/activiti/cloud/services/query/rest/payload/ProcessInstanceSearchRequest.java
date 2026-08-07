e032597770c1bce042e1cabf9a578c191d094aa5
elena.grosu <elena.grosu@hyland.com>
2026-08-07 12:25:33 +0200

    AAE-49542 Add hashCode and equals to ProcessInstanceSearchRequest

--- activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/payload/ProcessInstanceSearchRequest.java
+++ activiti-cloud-query-service/activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/payload/ProcessInstanceSearchRequest.java
@@ -17,6 +17,7 @@ package org.activiti.cloud.services.query.rest.payload;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Date;
+import java.util.Objects;
 import java.util.Set;
 import org.activiti.api.process.model.ProcessInstance;
 import org.activiti.cloud.services.query.model.ProcessVariableKey;
@@ -326,5 +327,73 @@ public class ProcessInstanceSearchRequest implements CloudRuntimeEntityFilterReq
 
     public void setSubprocessParentIds(Set<String> subprocessParentIds) {
         this.subprocessParentIds = subprocessParentIds;
+    }
+
+    // subprocessParentIds is derived server-side and mutated after the count cache key is built, so it is excluded
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ProcessInstanceSearchRequest that = (ProcessInstanceSearchRequest) o;
+        return (
+            Objects.equals(id, that.id) &&
+            Objects.equals(parentId, that.parentId) &&
+            Objects.equals(name, that.name) &&
+            Objects.equals(processDefinitionName, that.processDefinitionName) &&
+            Objects.equals(initiator, that.initiator) &&
+            Objects.equals(appVersion, that.appVersion) &&
+            Objects.equals(status, that.status) &&
+            Objects.equals(lastModifiedFrom, that.lastModifiedFrom) &&
+            Objects.equals(lastModifiedTo, that.lastModifiedTo) &&
+            Objects.equals(startFrom, that.startFrom) &&
+            Objects.equals(startTo, that.startTo) &&
+            Objects.equals(completedFrom, that.completedFrom) &&
+            Objects.equals(completedTo, that.completedTo) &&
+            Objects.equals(suspendedFrom, that.suspendedFrom) &&
+            Objects.equals(suspendedTo, that.suspendedTo) &&
+            Objects.equals(processVariableFilters, that.processVariableFilters) &&
+            Objects.equals(processVariableKeys, that.processVariableKeys) &&
+            Objects.equals(sort, that.sort) &&
+            Objects.equals(includeSubprocesses, that.includeSubprocesses) &&
+            Objects.equals(linkedProcessInstanceId, that.linkedProcessInstanceId) &&
+            Objects.equals(linkedProcessInstanceType, that.linkedProcessInstanceType) &&
+            Objects.equals(processRelatedTo, that.processRelatedTo) &&
+            Objects.equals(includeUnlinkedProcesses, that.includeUnlinkedProcesses) &&
+            Objects.equals(includeLinkedProcesses, that.includeLinkedProcesses)
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            id,
+            parentId,
+            name,
+            processDefinitionName,
+            initiator,
+            appVersion,
+            status,
+            lastModifiedFrom,
+            lastModifiedTo,
+            startFrom,
+            startTo,
+            completedFrom,
+            completedTo,
+            suspendedFrom,
+            suspendedTo,
+            processVariableFilters,
+            processVariableKeys,
+            sort,
+            includeSubprocesses,
+            linkedProcessInstanceId,
+            linkedProcessInstanceType,
+            processRelatedTo,
+            includeUnlinkedProcesses,
+            includeLinkedProcesses
+        );
     }
 }
