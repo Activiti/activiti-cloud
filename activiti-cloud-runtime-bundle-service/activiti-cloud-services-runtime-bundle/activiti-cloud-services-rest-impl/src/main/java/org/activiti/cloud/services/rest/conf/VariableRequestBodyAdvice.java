8536e20767789797c2d2b6184829753df55cac56
revatilimaye <revati.limaye@hyland.com>
2026-09-04 12:38:09 +0530

    AAE-50850 fix compilation error

--- activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/main/java/org/activiti/cloud/services/rest/conf/VariableRequestBodyAdvice.java
+++ activiti-cloud-runtime-bundle-service/activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/main/java/org/activiti/cloud/services/rest/conf/VariableRequestBodyAdvice.java
@@ -23,11 +23,11 @@ import org.activiti.api.process.model.payloads.SetProcessVariablesPayload;
 import org.activiti.api.task.model.payloads.CreateTaskVariablePayload;
 import org.activiti.api.task.model.payloads.UpdateTaskVariablePayload;
 import org.activiti.cloud.services.core.validation.VariableProperties;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.method.support.MethodParameter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 
 @ControllerAdvice
@@ -58,7 +58,7 @@ public class VariableRequestBodyAdvice extends RequestBodyAdviceAdapter {
         MethodParameter parameter,
         Type targetType,
         Class<? extends HttpMessageConverter<?>> converterType
-    ) {
+    ) throws IOException {
         InputStream body = new BoundedInputStream(inputMessage.getBody(), variableProperties.getMaxRequestSize());
         return new HttpInputMessage() {
             @Override
